9fd985cc8e9c7a722bd38f7b7f54e474fb1e85a9
Gabriel Inchauspe <ginchauspe@developmentgateway.org>
2016-11-18 15:12:19 -0300

    AMPOFFLINE-1: Removed HomePage component, moved some files and calling login form on "/" route.

--- app/components/layout/Home.js
+++ app/components/layout/Home.js
@@ -1,8 +1,8 @@
 // @flow
 import React, {Component} from 'react';
-import Login from './Login';
+import Login from '../login/Login';
 import LandingPageContent from './LandingPageContent';
-import Footer from '../layout/Footer';
+import Footer from './Footer';
 import {Link} from 'react-router';
 import styles from './Home.css';
 
@@ -11,8 +11,6 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        {/*<Login/>*/}
-        <Link to={'/login'} className="btn btn-default">Login</Link>
         <LandingPageContent/>
         <hr/>
         <Footer/>
--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
 import LoginPage from './containers/LoginPage';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
-    <Route path="/login" component={LoginPage} />
+    <IndexRoute component={LoginPage} />
   </Route>
 );
