4096448488330d03462a84ec77dfd5060251bad7
Gabriel Inchauspe <ginchauspe@developmentgateway.org>
2016-11-22 16:13:04 -0300

    AMPOFFLINE-5: List available workspaces.

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -1,14 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import { hashHistory } from 'react-router';
-import { routerMiddleware, push } from 'react-router-redux';
+import {hashHistory} from 'react-router';
+import {routerMiddleware, push} from 'react-router-redux';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
 import * as loginActions from '../actions/login';
+import * as workspaceActions from '../actions/workspace';
 
 const actionCreators = {
   ...loginActions,
+  ...workspaceActions,
   push,
 };
 
