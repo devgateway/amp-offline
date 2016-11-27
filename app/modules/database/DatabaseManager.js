12accc9c567f3fa80bb45f48e5d7185a119aadc4
Gabriel Inchauspe <ginchauspe@developmentgateway.org>
2016-11-27 16:59:31 -0300

    AMPOFFLINE-22: Changes in the mockup database manager.

--- app/modules/database/DatabaseManager.js
+++ app/modules/database/DatabaseManager.js
@@ -1,16 +1,37 @@
 import Datastore from 'nedb';
+import DatabaseCollection from 'DatabaseCollection';
+import _ from 'underscore';
 
-const fileNamePrefix = '/amp_offline/';
+const fileNamePrefix = './database/';
 const fileNameExtension = '.db';
 
 /**
+ * Is a Singleton to centralize control over the database access.
  * TODO: this class should be part of an API to connect to different databases (like NeDB).
  * @type {{connect: ((name, callback, options)), disconnect: ((name, callback, options)), createCollection: ((name, callback, options)), destroyCollection: ((name, callback, options)), insert: ((object, callback, options)), remove: ((object, callback, options)), find: ((object, callback, options))}}
  */
-const DatabaseManager = {
+class DatabaseManager {
+
+  // This variable will keep a list of open datastores to avoid opening twice.
+  collections = [];
+
+  //VERY IMPORTANT: NeDB can execute 1 operation at the same time and the rest is queued, so we always work async.
+  //VERY IMPORTANT 2: Loading the same datastore more than once didnt throw an error but drastically increased the MEM use.
+  //VERY IMPORTANT 3: Using a filename like '/path/to/database' will create a directory in your disk root.
+  //VERY IMPORTANT 4: A 60MB datastore file can use an average of 350MB with spikes of 800MB when opening (tested with 1M small documents).
+
+  constructor() {
+    console.log('constructor');
+    if (!DatabaseManager.instance) {
+      console.log('Reuse instance');
+      DatabaseManager.instance = this;
+    }
+    return DatabaseManager.instance;
+  };
 
   createCollectionAndConnect(name, callback, options) {
     console.log('createCollectionAndConnect');
+    //TODO: check if datastore is alredy opened/being opened.
     const db = new Datastore({filename: fileNamePrefix + name + fileNameExtension});
     db.loadDatabase(function (err) {
       if (err !== undefined) {
@@ -19,23 +40,30 @@ const DatabaseManager = {
         callback(true, db);
       }
     });
-  },
+  };
 
   insert(object, callback, options) {
     console.log('insert');
     callback(true);
-  },
+  };
 
   remove(object, callback, options) {
     console.log('remove');
     callback(true);
-  },
+  };
 
   find(object, callback, options) {
     console.log('find');
     callback(true);
+  };
+
+  checkIfCollectionIsOpened(name) {
+    //TODO: look for this collection in 'collections' and return it.
   }
 
-};
+}
 
-module.exports = DatabaseManager;
+const instance = new DatabaseManager();
+Object.freeze(instance);
+
+export default instance;
