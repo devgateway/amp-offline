f3a6a3073c8245ca1de61453eb922b87be93d51b
Julian de Anquin <jdeanquin@developmentgateway.org>
2016-12-22 19:29:29 -0300

    AMPOFFLINE-114 Initial ws list. Also an initial desktop page to test the links of the table TODO grouping by ws type

---
+++ app/actions/DesktopAction.js
@@ -0,0 +1,34 @@
+// @flow
+import UrlUtils from '../utils/URLUtils'
+
+
+//Types of redux actions
+export const STATE_DESKTOP_LOADING = 'STATE_DESKTOP_LOADING';
+export const STATE_DESKTOP_LOADED = 'STATE_DESKTOP_LOADING';
+
+//this will change its just a mockup
+export function loadDesktop(teamId) {
+  console.log('loadDesktop');
+  return (dispatch, ownProps) => {
+    if (ownProps().syncUp.loadingSyncHistory === false) {
+      console.log('loading desktop');
+      //this is to simulate loading activities in desktop
+      setTimeout(_loadDesktop(teamId), 5000);
+      let desktopData = {teamId: teamId};
+      return {
+        type: STATE_DESKTOP_LOADED,
+        actionData: desktopData,
+      }
+    }
+    dispatch(sendingRequest());
+  };
+}
+function _loadDesktop(teamId) {
+
+}
+function sendingRequest() {
+  console.log('sendingRequestLoadingDesktop');
+  return {
+    type: STATE_DESKTOP_LOADING
+  }
+}
---
+++ app/containers/DesktopPage.js
@@ -0,0 +1,17 @@
+// @flow
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import Desktop from '../components/desktop/Desktop';
+import * as DesktopActions from '../actions/DesktopAction';
+
+function mapStateToProps(state) {
+  console.log('mapStateToProps');
+  return state;
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  console.log('mapDispatchToProps');
+  return bindActionCreators(DesktopActions, dispatch, ownProps);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Desktop);
