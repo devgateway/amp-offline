4227483227b327ad507b1709d60e5390be96c210
Gabriel Inchauspe <ginchauspe@gmail.com>
2016-12-27 15:09:40 -0300

    AMPOFFLINE-72: Making menu generator truly dynamic.

--- app/utils/MenuUtils.js
+++ app/utils/MenuUtils.js
@@ -13,50 +13,39 @@ const menuUtils = {
     }
   },
 
-  //TODO: make this menu work with N levels.
+  generateTree(object, key, level, node){
+    let self = this;
+    const menuTrnPrefix = "menu";
+    if (object.nodes) {
+      node[level] = [];
+      Object.keys(object.nodes).forEach(function (key) {
+        node[level].push(self.generateTree(object.nodes[key], key, level + 1, node));
+      });
+      return (<SubMenu title={translate(menuTrnPrefix + '.' + key)} key={key}>{node[level]}</SubMenu>);
+    } else {
+      return (<MenuItem title={translate(menuTrnPrefix + '.' + key)} key={key}
+                        route={object.route}>{translate(menuTrnPrefix + '.' + key)}</MenuItem>);
+    }
+  },
+
   buildMenu(loggedIn) {
     const defaultMenu = require('../conf/menu.json');
     let topLevelMenu;
     let self = this;
-    const menuTrnPrefix = 'menu';
     let firstLevelEntries = [];
-    Object.keys(defaultMenu.menu).forEach(function (key) {
-      let obj = defaultMenu.menu[key];
-      let secondLevelEntries = [];
-      if (obj.nodes) {
-        Object.keys(obj.nodes).forEach(function (key2) {
-          let thirdLevelEntries = [];
-          let subMenu = false;
-          if (obj.nodes[key2].nodes) {
-            subMenu = true;
-            Object.keys(obj.nodes[key2].nodes).forEach(function (key3) {
-              if (self.checkIfPublic(obj.nodes[key2].nodes[key3].public, loggedIn)) {
-                thirdLevelEntries.push(<MenuItem key={key3}
-                                                 route={obj.nodes[key2].nodes[key3].route}>{translate(menuTrnPrefix + '.' + key3)}</MenuItem>);
-              }
-            });
-          } else {
-            if (self.checkIfPublic(obj.nodes[key2].public, loggedIn)) {
-              thirdLevelEntries = translate(menuTrnPrefix + '.' + key2);
-            }
-          }
-          if (self.checkIfPublic(obj.nodes[key2].public, loggedIn)) {
-            if (subMenu) {
-              secondLevelEntries.push(<SubMenu title={translate(menuTrnPrefix + '.' + key2)}
-                                               key={key2}>{thirdLevelEntries}</SubMenu>);
-            } else {
-              secondLevelEntries.push(<MenuItem key={key2}
-                                                route={obj.nodes[key2].route}>{thirdLevelEntries}</MenuItem>);
-            }
-          }
-        });
-      }
-      if (self.checkIfPublic(obj.public, loggedIn)) {
-        firstLevelEntries.push((
-          <SubMenu title={<span>{translate(menuTrnPrefix + '.' + key)}</span>}
-                   key={key}>{secondLevelEntries}</SubMenu>));
-      }
-    });
+    if (defaultMenu.menu !== undefined && defaultMenu.menu !== null) {
+      let currentLevel = 0;
+      // Iterate first level items.
+      Object.keys(defaultMenu.menu).forEach(function (key) {
+        let firstLevelObject = defaultMenu.menu[key];
+        if (self.checkIfPublic(firstLevelObject.public, loggedIn)) {
+          let structure = self.generateTree(firstLevelObject, key, 0, []);
+          firstLevelEntries.push(structure);
+        }
+      });
+    } else {
+      console.warn("Empty menu");
+    }
     topLevelMenu = (<Menu onClick={self.handleClick}>{firstLevelEntries}</Menu>);
 
     return React.cloneElement(topLevelMenu, {
