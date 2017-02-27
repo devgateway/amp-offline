27a67ae16f27bff7d043b14200992f17ed2ab72d
Gabriel Inchauspe <ginchauspe@developmentgateway.org>
2017-02-27 19:56:53 -0300

    AMPOFFLINE-215: Cleaning up code.

--- app/components/desktop/IconFormatter.jsx
+++ app/components/desktop/IconFormatter.jsx
@@ -1,19 +1,29 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { ACTIVITY_PREVIEW_URL, ACTIVITY_EDIT_URL } from '../../utils/Constants';
+import translate from '../../utils/translate';
 
 export default class IconFormatter extends Component {
+
+  static propTypes = {
+    row: PropTypes.object.isRequired
+  };
 
   render() {
     // TODO: These links could be dispatch to some action too if needed.
     const editLink = ACTIVITY_EDIT_URL + '/' + this.props.row.ampId;
     const viewLink = ACTIVITY_PREVIEW_URL + '/' + this.props.row.ampId;
+    const iconWidth = 20;
     if (this.props.row.edit) {
-      return <a href={editLink}><img alt="edit" src="../resources/images/edit.svg" width={20}/></a>;
+      return (<a href={editLink} title={translate('Click to edit this activity.')}>
+        <img alt="edit" src="../resources/images/edit.svg" width={iconWidth}/>
+      </a>);
     }
     if (this.props.row.view) {
-      return <a href={viewLink}><img alt="view" src="../resources/images/view.svg" width={20}/></a>;
+      return (<a href={viewLink} title={translate('Click to preview this activity.')}>
+        <img alt="view" src="../resources/images/view.svg" width={iconWidth}/>
+      </a>);
     }
-    return <span></span>;
+    return <span/>;
   }
 }
--- app/components/desktop/LinkFormatter.jsx
+++ app/components/desktop/LinkFormatter.jsx
@@ -1,14 +1,19 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { ACTIVITY_PREVIEW_URL } from '../../utils/Constants';
+import translate from '../../utils/translate';
 
 export default class LinkFormatter extends Component {
+
+  static propTypes = {
+    row: PropTypes.object.isRequired
+  };
 
   render() {
     // TODO: This link could be dispatch to some action too if needed.
     const link = ACTIVITY_PREVIEW_URL + '/' + this.props.row.ampId;
     return (
-      <a href={link}>{this.props.row.title}</a>
+      <a href={link} title={translate('Click to preview this activity.')}>{this.props.row.title}</a>
     );
   }
 }
