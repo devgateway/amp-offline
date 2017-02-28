78f0ceb991896105bb69fcd61d0d4e388b2ef084
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2017-02-28 11:59:48 +0200

    AMPOFFLINE-47 implement retry logic (#22)
    * AMPOFFLINE-47 implement retry logic
    
    * AMPOFFLINE-47 implement selective retry
    
    * AMPOFFLINE-47 Added should retry check
    
    * AMPOFFLINE-47 code review changes
    
    * AMPOFFLINE-47 code review changes
    
    * AMPOFFLINE-47 exclude 2xx y 3xx codes

--- app/modules/connectivity/ConnectionHelper.js
+++ app/modules/connectivity/ConnectionHelper.js
@@ -1,22 +1,23 @@
-import request from "request";
-import RequestConfig from "./RequestConfig";
-import Notification from "../helpers/NotificationHelper";
+import request from 'request';
+import RequestConfig from './RequestConfig';
+import Notification from '../helpers/NotificationHelper';
+import { MAX_RETRY_ATEMPTS, ERRORS_TO_RETRY } from '../../utils/Constants';
 import {
   NOTIFICATION_ORIGIN_API_SECURITY,
   NOTIFICATION_ORIGIN_API_NETWORK,
   NOTIFICATION_SEVERITY_ERROR
-} from "../../utils/constants/ErrorConstants";
-import { store } from "../../index";
-import { loginAutomaticallyAction, logoutAction } from "../../actions/LoginAction";
-import translate from "../../utils/translate";
+} from '../../utils/constants/ErrorConstants';
+import { store } from '../../index';
+import { loginAutomaticallyAction, logoutAction } from '../../actions/LoginAction';
+import translate from '../../utils/translate';
 const ConnectionHelper = {
-
-  doGet({ url, paramsMap }) {
+  doGet({ url, paramsMap, shouldRetry }) {
     console.log('doGet');
     const method = 'GET';
     // Modify the call to use ES6 destructuring
     const requestConfig = RequestConfig.getRequestConfig({ method, url, paramsMap });
-    return this._doMethod(requestConfig);
+    const maxRetryAttemps = MAX_RETRY_ATEMPTS;
+    return this._doMethod(requestConfig, maxRetryAttemps, shouldRetry);
   },
 
   /**
@@ -26,25 +27,43 @@ const ConnectionHelper = {
    * @param body
    * @returns {Promise}
    */
-  doPost({ url, paramsMap, body }) {
+  doPost({ url, paramsMap, body, shouldRetry }) {
     console.log('doPost');
     // Notice that we are actually receiving an object as a parameter  but we are destructuring it
     const method = 'POST';
     const requestConfig = RequestConfig.getRequestConfig({ method, url, paramsMap, body });
-    return this._doMethod(requestConfig);
+    const maxRetryAttemps = MAX_RETRY_ATEMPTS;
+    return this._doMethod(requestConfig, maxRetryAttemps, shouldRetry);
   },
 
-  _doMethod(options) {
+  _doMethod(requestConfig, maxRetryAttemps, shouldRetry) {
     console.log('_doMethod');
     const self = this;
     return new Promise((resolve, reject) => {
-      request(options, (error, response, body) => {
-        if (error || response.statusCode !== 200 || body.error) {
-          if (response && response.statusCode === 401) {
+      request(requestConfig, (error, response, body) => {
+        if (error ||  !(response.statusCode >= 200 &&  response.statusCode < 400 )||  body.error) {
+          const shouldRetryOnError = ERRORS_TO_RETRY.filter((value) => {
+            return value === error.code;
+          });
+          if (shouldRetryOnError.length > 0) {
+            if (maxRetryAttemps > 0 && shouldRetry) {
+
+              return this._doMethod(requestConfig, --maxRetryAttemps, shouldRetry).then(resolve).catch(reject);
+
+            } else {
+              const notifErrorTimeout = new Notification({
+                errorObject: error,
+                origin: NOTIFICATION_ORIGIN_API_NETWORK,
+                severity: NOTIFICATION_SEVERITY_ERROR,
+                message: error.code
+              });
+              reject(notifErrorTimeout);
+            }
+          } else if (response && response.statusCode === 401) {
             // Lets try to relogin online automatically (https://github.com/reactjs/redux/issues/974)
             store.dispatch(loginAutomaticallyAction()).then((data) => {
-              options = RequestConfig.replaceToken(options);
-              self._doMethod(options).then((body_) => {
+              const options_ = RequestConfig.replaceToken(requestConfig);
+              self._doMethod(options_).then((body_) => {
                 resolve(body_);
               }).catch((error_) => {
                 // If we couldnt relogin online automatically we logout completely and forward to login page.
@@ -68,6 +87,7 @@ const ConnectionHelper = {
               origin: NOTIFICATION_ORIGIN_API_NETWORK,
               severity: NOTIFICATION_SEVERITY_ERROR
             }));
+            reject(notifErro2);
           }
         } else {
           resolve(body);
