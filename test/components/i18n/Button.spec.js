206a343d8d1cc687de23db29ff0d057f78821057
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-03-10 23:53:19 +0200

    AMPOFFLINE-170 fixing code style issues

--- test/components/i18n/Button.spec.js
+++ test/components/i18n/Button.spec.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-expressions */
-import {expect} from 'chai';
-import {spy} from 'sinon';
+import { expect } from 'chai';
+import { spy } from 'sinon';
 import React from 'react';
-import {shallow} from 'enzyme';
-import Button from '../../../app/components/i18n/Button.jsx';
+import { shallow } from 'enzyme';
+import { describe, it } from 'mocha';
+import Button from '../../../app/components/i18n/Button';
 import translate from '../../../app/utils/translate';
 
 function setup() {
@@ -12,8 +13,10 @@ function setup() {
     text: 'test',
     className: 'test-class'
   };
-  const component = shallow(<Button text={props.text} onClick={props.onClick}
-                                    className={props.className}/>);
+  const component = shallow(
+    <Button
+      text={props.text} onClick={props.onClick} className={props.className}
+    />);
   return {
     component,
     props,
@@ -23,19 +26,25 @@ function setup() {
 
 
 describe('@@ Button @@', () => {
-  it('Should translate correctly the text props.', () => {
-    const {buttons, props} = setup();
-    expect(buttons.at(0).text()).to.equal(translate(props.text));
-  });
+  describe('text', () =>
+    it('should translate correctly the text props.', () => {
+      const { buttons, props } = setup();
+      expect(buttons.at(0).text()).to.equal(translate(props.text));
+    })
+  );
 
-  it('Should use the text class from props.', () => {
-    const {component, props} = setup();
-    expect(component.find('.' + props.className)).to.have.length(1);
-  });
+  describe('find', () =>
+    it('should use the text class from props.', () => {
+      const { component, props } = setup();
+      expect(component.find(`.${props.className}`)).to.have.length(1);
+    })
+  );
 
-  it('Should use the click handler from props.', () => {
-    const {buttons, props} = setup();
-    buttons.at(0).simulate('click');
-    expect(props.onClick.called).to.be.true;
-  });
+  describe('click', () =>
+    it('should use the click handler from props.', () => {
+      const { buttons, props } = setup();
+      buttons.at(0).simulate('click');
+      expect(props.onClick.called).to.be.true;
+    })
+  );
 });
