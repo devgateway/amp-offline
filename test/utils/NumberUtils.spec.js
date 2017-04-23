bfbb6f1e5f6ec04e57e394852a7026c621cf6095
Gabriel Inchauspe <ginchauspe@developmentgateway.org>
2017-04-23 16:46:20 -0300

    AMPOFFLINE-129: First tests.

---
+++ test/utils/NumberUtils.spec.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Gabriel on 23/04/2017.
+ */
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { expect } from 'chai';
+import NumberUtils from '../../app/utils/NumberUtils';
+import { GS_AMOUNT_OPTION_IN_UNITS } from '../../app/utils/constants/GlobalSettingConstants';
+import { STATE_GS_NUMBERS_LOADED } from '../../app/actions/StartUpAction';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const noDecimalConfig = {
+  decimalSeparator: '@', // choose non-standar options to test for false negatives.
+  groupSeparator: '&', // idem.
+  format: '###.###',
+  amountsInThousands: GS_AMOUNT_OPTION_IN_UNITS
+};
+
+describe('@@ Numbertils @@', () => {
+  it('should return a config', () => {
+    expect(NumberUtils.getConfigFromDB())
+      .to.have.property('decimalSeparator', 'groupSeparator', 'format', 'amountsInThousands');
+  });
+
+  it('should convert numbers', () => {
+    mockStore.dispatch({ type: STATE_GS_NUMBERS_LOADED, actionData: noDecimalConfig });
+    NumberUtils.createLanguage();
+    expect(NumberUtils.rawNumberToFormattedString(1)).to.equal('1');
+    expect(NumberUtils.rawNumberToFormattedString(10)).to.equal('10');
+    expect(NumberUtils.rawNumberToFormattedString(100)).to.equal('100');
+    expect(NumberUtils.rawNumberToFormattedString(1000)).to.equal('1&000');
+    expect(NumberUtils.rawNumberToFormattedString(20000)).to.equal('20&000');
+    expect(NumberUtils.rawNumberToFormattedString(300000)).to.equal('300&000');
+    expect(NumberUtils.rawNumberToFormattedString(4000000)).to.equal('4&000&000');
+    expect(NumberUtils.rawNumberToFormattedString(50000000)).to.equal('50&000&000');
+  });
+});
