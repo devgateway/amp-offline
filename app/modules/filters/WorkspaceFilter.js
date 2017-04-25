ccdec5d8378f2a354c24cf8c83351236bc148932
nmandrescu <nmandrescu@developmentgateway.org>
2017-04-25 16:50:15 +0300

    task/AMPOFFLINE-179/ws-filters-updates (#106)
    * AMPOFFLINE-179 integrating ws filters in tabs
    
    * AMPOFFLINE-179 integrating ws filters in tabs
    
    * AMPOFFLINE-179 implement GS TODOs, dynamic dates won't be implemented
    
    * AMPOFFLINE-179 TODOs updates
    
    * AMPOFFLINE-179 move WS filters
    
    * AMPOFFLINE-179 add workspace filters unit tests

--- app/modules/filters/WorkspaceFilter.js
+++ app/modules/filters/WorkspaceFilter.js
@@ -1,10 +1,8 @@
-// TODO: this is temporary to move on and final binding to the store will be done through AMPOFFLINE-103
-import store from '../../index';
-import WorkspaceHelper from '../helpers/WorkspaceHelper';
+import * as WorkspaceHelper from '../helpers/WorkspaceHelper';
 import Utils from '../../utils/Utils';
 import * as ActivityConstants from '../../utils/constants/ActivityConstants';
-import ActivityFilter from '../filters/ActivityFilter';
-import LoggerManager from '../../modules/util/LoggerManager';
+import ActivityFilter from './ActivityFilter';
+import LoggerManager from '../util/LoggerManager';
 
 /**
  * Workspace Filter class
@@ -12,11 +10,12 @@ import LoggerManager from '../../modules/util/LoggerManager';
  * @author Nadejda Mandrescu
  */
 export default class WorkspaceFilterBuilder {
+  static getDBFilter(workspace) {
+    return (new WorkspaceFilterBuilder(workspace)).getDBFilter();
+  }
 
-  constructor() {
-    this._dbFilter = {};
-    this._filterBuilt = false;
-    this._workspace = store.getState().workspace.currentWorkspace;
+  constructor(workspace) {
+    this._workspace = workspace;
     this._isComputed = this._workspace && this._workspace['is-computed'] === true;
     this._wsFilters = this._workspace ? this._workspace['workspace-filters'] : undefined;
   }
@@ -27,17 +26,11 @@ export default class WorkspaceFilterBuilder {
    */
   getDBFilter() {
     LoggerManager.log('getDBFilter');
-    const self = this;
     return new Promise((resolve, reject) => {
-      if (self._workspace && !self._filterBuilt) {
-        self._filterBuilt = true;
-        self._dbFilter = self._prepareFilters().then(() => {
-          self._dbFilter = self._generateDBFilter();
-          return resolve(self._dbFilter);
-        }).catch(reject);
-      } else {
-        resolve(self._dbFilter);
+      if (!this._workspace) {
+        return resolve({});
       }
+      return this._prepareFilters().then(() => resolve(this._generateDBFilter())).catch(reject);
     });
   }
 
@@ -69,7 +62,7 @@ export default class WorkspaceFilterBuilder {
   _generateDBFilter() {
     LoggerManager.log('_generateDBFilter');
     // initialise the team filter (no special Management workspaces rules are needed)
-    const teamFilter = Utils.toMap(ActivityConstants.TEAM, { $eq: this._workspace.id });
+    const teamFilter = Utils.toMap(ActivityConstants.TEAM, this._workspace.id);
     // non-computed workspace filter
     let dbFilter = teamFilter;
     // Add computed filters if needed
@@ -92,17 +85,17 @@ export default class WorkspaceFilterBuilder {
     LoggerManager.log('_getComputedOrgsFilter');
     let computedOrgsFilter;
     const orgIds = this._workspace.organizations;
-    if (orgIds && orgIds.length() > 0) {
+    if (orgIds && orgIds.length > 0) {
       // build activity orgs filter
       const activityOrgs = [];
       ActivityConstants.ORG_ROLE_FIELDS.forEach(orgField => {
         const orgFilter = Utils.toMap(ActivityConstants.ORG_ROLE_ORG_ID, { $in: orgIds });
         const orgRoleFilter = Utils.toMap(orgField, { $elemMatch: orgFilter });
-        activityOrgs.add(orgRoleFilter);
+        activityOrgs.push(orgRoleFilter);
       });
       const fundingDonorOrgFilter = Utils.toMap(ActivityConstants.FUNDING_DONOR_ORG_ID, { $in: orgIds });
       const fundingOrgs = Utils.toMap(ActivityConstants.FUNDINGS, { $elemMatch: fundingDonorOrgFilter });
-      activityOrgs.add(fundingOrgs);
+      activityOrgs.push(fundingOrgs);
       // add draft flag if needed
       if (this._workspace['hide-draft'] === true) {
         const isDraftFilter = Utils.toMap(ActivityConstants.IS_DRAFT, { $ne: true });
---
+++ app/utils/constants/GlobalSettingsConstants.js
@@ -0,0 +1,2 @@
+export const SHOW_WORKSPACE_FILTER_KEY = 'Show workspace filter';
+export const FILTER_BY_DATE_HIDE_PROJECTS = 'Filter by date hides projects';
