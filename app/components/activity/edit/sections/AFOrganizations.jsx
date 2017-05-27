a7115c8f1e5dedc4b8bfc330af96eceb95f8fae3
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-05-27 14:54:57 -0300

    AMPOFFLINE-434: Showing each option.

--- app/components/activity/edit/sections/AFOrganizations.jsx
+++ app/components/activity/edit/sections/AFOrganizations.jsx
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
 import AFSection from './AFSection';
+import AFField from '../components/AFField';
 import { ORGANIZATIONS } from './AFSectionConstants';
+import * as AC from '../../../../utils/constants/ActivityConstants';
 import LoggerManager from '../../../../modules/util/LoggerManager';
+import afStyles from '../ActivityForm.css';
 
 /**
  * Organizations Section
@@ -19,7 +23,35 @@ class AFOrganizations extends Component {
   }
 
   render() {
-    return <div>TODO</div>;
+    return (<div className={afStyles.full_width}>
+      <Grid className={afStyles.full_width}>
+        <Row>
+          <Col md={12} lg={12}>
+            <AFField parent={this.props.activity} fieldPath={AC.RESPONSIBLE_ORGANIZATION} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} lg={12}>
+            <AFField parent={this.props.activity} fieldPath={AC.EXECUTING_AGENCY} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} lg={12}>
+            <AFField parent={this.props.activity} fieldPath={AC.IMPLEMENTING_AGENCY} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} lg={12}>
+            <AFField parent={this.props.activity} fieldPath={AC.BENEFICIARY_AGENCY} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} lg={12}>
+            <AFField parent={this.props.activity} fieldPath={AC.CONTRACTING_AGENCY} />
+          </Col>
+        </Row>
+      </Grid>
+    </div>);
   }
 }
 
