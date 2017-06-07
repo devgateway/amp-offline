d3e2ebd9cabc8eccbb50c7a29a9f49fc3d8ea1bb
Alexei Savca <savca.alexei@gmail.com>
2017-06-07 22:32:53 +0300

    AMPOFFLINE-192 Exiting the messages

--- app/components/notifications/message.jsx
+++ app/components/notifications/message.jsx
@@ -2,8 +2,11 @@ import { Alert } from 'react-bootstrap';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Notification from '../../modules/helpers/NotificationHelper';
+import styles from './style.css';
 
-const TIMEOUT = 10 * 1000;
+const TIMEOUT = 10 * 1000;//total amount of time the message stays visible
+//when it's this amount of time left, we nofity the user that the message is about to disappear
+const DISAPPEAR_TIMEOUT = TIMEOUT / 2;
 const CHECK_INTERVAL = 100;
 
 class Message extends PureComponent {
@@ -16,7 +19,8 @@ class Message extends PureComponent {
     super(...args);
     this.state = {
       timeLeft: TIMEOUT,
-      isHovered: false
+      isHovered: false,
+      isUnmounting: false
     };
   }
 
@@ -26,7 +30,9 @@ class Message extends PureComponent {
       const { timeLeft, isHovered } = this.state;
       const newTimeLeft = timeLeft - CHECK_INTERVAL;
       if (newTimeLeft <= 0) {
-        onDismiss();
+        clearInterval(this.removalInterval);
+        this.setState({ isUnmounting: true });
+        setTimeout(onDismiss, 1000);
       } else if (!isHovered) {
         this.setState({ timeLeft: newTimeLeft });
       }
@@ -37,17 +43,31 @@ class Message extends PureComponent {
     clearInterval(this.removalInterval);
   }
 
+  getOpacity() {
+    const { timeLeft, isHovered } = this.state;
+    if (isHovered || (timeLeft > DISAPPEAR_TIMEOUT)){
+      return 1;
+    } else {
+      return timeLeft / DISAPPEAR_TIMEOUT;
+    }
+  }
+
   render() {
     const { notification, onDismiss } = this.props;
+    const { isUnmounting } = this.state;
+    let classes = [styles.alert_wrapper];
+    if (isUnmounting) classes.push(styles.unmounting);
     return (
-      <Alert
-        onDismiss={onDismiss}
-        onMouseEnter={() => this.setState({isHovered: true})}
-        onMouseLeave={() => this.setState({isHovered: false})}
-        onClick={() => clearTimeout(this.removalInterval)}
-      >
-        {notification.message}
-      </Alert>
+      <div style={{opacity: this.getOpacity()}} className={classes.join(' ')}>
+        <Alert
+          onDismiss={onDismiss}
+          onMouseEnter={() => this.setState({isHovered: true})}
+          onMouseLeave={() => this.setState({isHovered: false})}
+          onClick={() => clearTimeout(this.removalInterval)}
+        >
+          {notification.message}
+        </Alert>
+      </div>
     );
   }
 }
