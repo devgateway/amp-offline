d37590f06fa7a2fb40e22e40d1fbf962f644b2ee
Alexei Savca <savca.alexei@gmail.com>
2017-06-08 00:12:47 +0300

    AMPOFFLINE-192 Added enter transitions

--- app/components/notifications/message.jsx
+++ app/components/notifications/message.jsx
@@ -4,10 +4,14 @@ import PropTypes from 'prop-types';
 import Notification from '../../modules/helpers/NotificationHelper';
 import styles from './style.css';
 
-const TIMEOUT = 10 * 1000;//total amount of time the message stays visible
-//when it's this amount of time left, we nofity the user that the message is about to disappear
+const TIMEOUT = 10 * 1000;// total amount of time the message stays visible
+// when it's this amount of time left, we nofity the user that the message is about to disappear
 const DISAPPEAR_TIMEOUT = TIMEOUT / 2;
 const CHECK_INTERVAL = 100;
+// how long should the enter/leave animation run(ms)
+// (if you change this you'll wanna change it in style.css, too)
+const ANIMATION_DURATION = 500;
+
 
 class Message extends PureComponent {
   static propTypes = {
@@ -20,23 +24,25 @@ class Message extends PureComponent {
     this.state = {
       timeLeft: TIMEOUT,
       isHovered: false,
-      isUnmounting: false
+      isMounting: false,
+      isUnmounting: false,
+      wasDismissedByUser: false,
+      wasStoppedByUser: false
     };
   }
 
   componentDidMount() {
-    const { onDismiss } = this.props;
     this.removalInterval = setInterval(() => {
       const { timeLeft, isHovered } = this.state;
       const newTimeLeft = timeLeft - CHECK_INTERVAL;
       if (newTimeLeft <= 0) {
-        clearInterval(this.removalInterval);
-        this.setState({ isUnmounting: true });
-        setTimeout(onDismiss, 1000);
+        this.dismiss();
       } else if (!isHovered) {
         this.setState({ timeLeft: newTimeLeft });
       }
     }, CHECK_INTERVAL);
+
+    setTimeout(() => this.setState({ isMounting: true }));
   }
 
   componentWillUnmount() {
@@ -44,26 +50,45 @@ class Message extends PureComponent {
   }
 
   getOpacity() {
-    const { timeLeft, isHovered } = this.state;
-    if (isHovered || (timeLeft > DISAPPEAR_TIMEOUT)){
+    const { timeLeft, isHovered, wasStoppedByUser } = this.state;
+    if (isHovered || wasStoppedByUser || (timeLeft > DISAPPEAR_TIMEOUT)) {
       return 1;
     } else {
       return timeLeft / DISAPPEAR_TIMEOUT;
     }
   }
 
+  dismiss() {
+    const { onDismiss } = this.props;
+    clearInterval(this.removalInterval);
+    this.setState({ isUnmounting: true });
+    setTimeout(onDismiss, ANIMATION_DURATION);
+  }
+
+  handleUserDismissal() {
+    this.setState({ wasDismissedByUser: true });
+    setTimeout(() => this.dismiss(), ANIMATION_DURATION);
+  }
+
+  stoppedByUser() {
+    clearInterval(this.removalInterval);
+    this.setState({ wasStoppedByUser: true });
+  }
+
   render() {
-    const { notification, onDismiss } = this.props;
-    const { isUnmounting } = this.state;
-    let classes = [styles.alert_wrapper];
+    const { notification } = this.props;
+    const { isMounting, isUnmounting, wasDismissedByUser } = this.state;
+    const classes = [styles.alert_wrapper];
+    if (isMounting) classes.push(styles.mounted);
     if (isUnmounting) classes.push(styles.unmounting);
+    if (wasDismissedByUser) classes.push(styles.user_dismissed);
     return (
-      <div style={{opacity: this.getOpacity()}} className={classes.join(' ')}>
+      <div style={{ opacity: this.getOpacity() }} className={classes.join(' ')}>
         <Alert
-          onDismiss={onDismiss}
-          onMouseEnter={() => this.setState({isHovered: true})}
-          onMouseLeave={() => this.setState({isHovered: false})}
-          onClick={() => clearTimeout(this.removalInterval)}
+          onDismiss={() => this.handleUserDismissal()}
+          onMouseEnter={() => this.setState({ isHovered: true })}
+          onMouseLeave={() => this.setState({ isHovered: false })}
+          onClick={() => this.stoppedByUser()}
         >
           {notification.message}
         </Alert>
