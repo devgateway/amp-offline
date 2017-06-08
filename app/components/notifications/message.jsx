7eb9cc568ed70f9c3ee5bfd2dc4155604355ab8f
Alexei Savca <savca.alexei@gmail.com>
2017-06-08 12:46:41 +0300

    AMPOFFLINE-192 Mapping Notification severity to bsStyle

--- app/components/notifications/message.jsx
+++ app/components/notifications/message.jsx
@@ -3,15 +3,20 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Notification from '../../modules/helpers/NotificationHelper';
 import styles from './style.css';
+import {
+  NOTIFICATION_SEVERITY_WARNING,
+  NOTIFICATION_SEVERITY_INFO,
+  NOTIFICATION_SEVERITY_ERROR
+} from '../../utils/constants/ErrorConstants';
 
-const TIMEOUT = 10 * 1000;// total amount of time the message stays visible
+// total amount of time the message stays visible
+const TIMEOUT = 10 * 1000;
 // when it's this amount of time left, we nofity the user that the message is about to disappear
-const DISAPPEAR_TIMEOUT = TIMEOUT / 2;
+const DISAPPEAR_TIMEOUT = TIMEOUT / 5;
 const CHECK_INTERVAL = 100;
 // how long should the enter/leave animation run(ms)
 // (if you change this you'll wanna change it in style.css, too)
 const ANIMATION_DURATION = 500;
-
 
 class Message extends PureComponent {
   static propTypes = {
@@ -49,6 +54,16 @@ class Message extends PureComponent {
     clearInterval(this.removalInterval);
   }
 
+  getBsStyle() {
+    const { notification } = this.props;
+    switch (notification.severity) {
+      case NOTIFICATION_SEVERITY_ERROR: return 'danger';
+      case NOTIFICATION_SEVERITY_WARNING: return 'warning';
+      case NOTIFICATION_SEVERITY_INFO: return 'info';
+      default: return 'success';
+    }
+  }
+
   getOpacity() {
     const { timeLeft, isHovered, wasStoppedByUser } = this.state;
     if (isHovered || wasStoppedByUser || (timeLeft > DISAPPEAR_TIMEOUT)) {
@@ -78,6 +93,7 @@ class Message extends PureComponent {
   render() {
     const { notification } = this.props;
     const { isMounting, isUnmounting, wasDismissedByUser } = this.state;
+
     const classes = [styles.alert_wrapper];
     if (isMounting) classes.push(styles.mounted);
     if (isUnmounting) classes.push(styles.unmounting);
@@ -89,6 +105,7 @@ class Message extends PureComponent {
           onMouseEnter={() => this.setState({ isHovered: true })}
           onMouseLeave={() => this.setState({ isHovered: false })}
           onClick={() => this.stoppedByUser()}
+          bsStyle={this.getBsStyle()}
         >
           {notification.message}
         </Alert>
