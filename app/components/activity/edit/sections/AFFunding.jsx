5430a7d89dc0fef4d8c8dec2586f5dd892c20e5d
Gabriel Inchauspe <ginchauspe@developmentgateway.org>
2017-06-10 18:52:36 -0300

    AMPOFFLINE-434: Adding basic funding tabs.

--- app/components/activity/edit/sections/AFFunding.jsx
+++ app/components/activity/edit/sections/AFFunding.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import * as ReactBootstrap from 'react-bootstrap';
 import AFSection from './AFSection';
 import { FUNDING } from './AFSectionConstants';
+import * as AC from '../../../../utils/constants/ActivityConstants';
 import LoggerManager from '../../../../modules/util/LoggerManager';
 
 /**
@@ -9,13 +11,30 @@ import LoggerManager from '../../../../modules/util/LoggerManager';
  */
 class AFFunding extends Component {
 
+  static propTypes = {
+    activity: PropTypes.object.isRequired
+  }
+
   constructor(props) {
     super(props);
     LoggerManager.log('constructor');
   }
 
+  generateFundingTabs() {
+    if (this.props.activity.fundings) {
+      return this.props.activity.fundings.map((funding) => (
+        <ReactBootstrap.Tab eventKey={funding[AC.AMP_FUNDING_ID]} title={funding[AC.FUNDING_DONOR_ORG_ID].value}>Tab 2
+          content</ReactBootstrap.Tab>
+      ));
+    }
+    return null;
+  }
+
   render() {
-    return <div>TODO</div>;
+    return (<ReactBootstrap.Tabs defaultActiveKey={0} onSelect={this.handleSelect} id="funding-tabs-container-tabs">
+      <ReactBootstrap.Tab eventKey={0} title="Overview">Overview</ReactBootstrap.Tab>
+      {this.generateFundingTabs()}
+    </ReactBootstrap.Tabs>);
   }
 }
 
