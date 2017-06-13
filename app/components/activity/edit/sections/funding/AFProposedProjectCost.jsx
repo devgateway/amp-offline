7b2477c2974576433b284a161d6fa869d9a5eba1
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-06-13 20:11:08 -0300

    AMPOFFLINE-434: Styles.

--- app/components/activity/edit/sections/funding/AFProposedProjectCost.jsx
+++ app/components/activity/edit/sections/funding/AFProposedProjectCost.jsx
@@ -4,48 +4,68 @@ import * as AC from '../../../../../utils/constants/ActivityConstants';
 import LoggerManager from '../../../../../modules/util/LoggerManager';
 import translate from '../../../../../utils/translate';
 import { createFormattedDate } from '../../../../../utils/DateUtils';
+import NumberUtils from '../../../../../utils/NumberUtils';
+import styles from '../../components/AFList.css';
 
 export default class AFProposedProjectCost extends Component {
 
   static propTypes = {
-    activity: PropTypes.array.isRequired
+    activity: PropTypes.object.isRequired
   };
 
   constructor(props) {
     super(props);
     LoggerManager.log('constructor');
+    this.options = {
+      withoutNoDataText: true
+    };
   }
 
   getCurrencyCodeSelector(cell) {
     // TODO: return the list from possible value 'ppc_amount~currency_code' in a combo.
-    return `<span>${cell.value}</span>`;
+    return (<span className={styles.editable}>{cell.value}</span>);
   }
 
   getDate(cell) {
-    return createFormattedDate(cell);
+    return (<span className={styles.editable}>{createFormattedDate(cell)}</span>);
+  }
+
+  getAmount(cell) {
+    return (<span className={styles.editable}>{NumberUtils.rawNumberToFormattedString(cell)}</span>);
   }
 
   render() {
+    const cellEdit = {
+      mode: 'click',
+      blurToSave: true
+    };
+
     if (this.props.activity[AC.PPC_AMOUNT]) {
       const columns = [<TableHeaderColumn dataField={AC.FUNDING_AMOUNT_ID} isKey hidden />];
       // TODO: check FM.
       if (true) {
-        columns.push(<TableHeaderColumn dataField={AC.AMOUNT}>{translate('Amount')}</TableHeaderColumn>);
+        columns.push(<TableHeaderColumn
+          dataField={AC.AMOUNT} editable
+          dataFormat={this.getAmount}>{translate('Amount')}</TableHeaderColumn>);
       }
       if (true) {
         columns.push(<TableHeaderColumn
-          dataField={[AC.CURRENCY_CODE]}
+          dataField={AC.CURRENCY_CODE} editable
           dataFormat={this.getCurrencyCodeSelector}>{translate('Currency')}</TableHeaderColumn>);
       }
       if (true) {
         columns.push(<TableHeaderColumn
-          dataField={AC.FUNDING_DATE}
+          dataField={AC.FUNDING_DATE} editable
           dataFormat={this.getDate}>{translate('Date')}</TableHeaderColumn>);
       }
-      return (<BootstrapTable
-        data={this.props.activity[AC.PPC_AMOUNT]} bordered={false} striped hover>
-        {columns}
-      </BootstrapTable>);
+      return (<div>
+        <BootstrapTable
+          options={this.options} containerClass={styles.containerTable} tableHeaderClass={styles.header}
+          thClassName={styles.thClassName} cellEdit={cellEdit} hover
+          data={this.props.activity[AC.PPC_AMOUNT]}>
+          {columns}
+        </BootstrapTable>
+      </div>);
     }
     return null;
   }
