f839a5a2c7543d3f7351d228ba4160b6cb50e30c
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-06-20 12:41:11 -0300

    AMPOFFLINE-434: Testing how to link data changes in table back to original activity.

--- app/components/activity/edit/sections/funding/AFProjectCost.jsx
+++ app/components/activity/edit/sections/funding/AFProjectCost.jsx
@@ -2,7 +2,7 @@
 import React, { Component, PropTypes } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import * as AC from '../../../../../utils/constants/ActivityConstants';
-import * as VC from '../../../../../utils/constants/ValueConstants';
+import * as FP from '../../../../../utils/constants/FieldPathConstants';
 import * as AF from '../../components/AFComponentTypes';
 import LoggerManager from '../../../../../modules/util/LoggerManager';
 import translate from '../../../../../utils/translate';
@@ -11,14 +11,19 @@ import NumberUtils from '../../../../../utils/NumberUtils';
 import styles from '../../components/AFList.css';
 import AFField from '../../components/AFField';
 import AFOverallFundingTotals from './AFOverallFundingTotals';
+import ActivityFieldsManager from '../../../../../modules/activity/ActivityFieldsManager';
 
 /**
  * @author Gabriel Inchauspe
  */
 export default class AFProjectCost extends Component {
 
+  static contextTypes = {
+    activityFieldsManager: PropTypes.instanceOf(ActivityFieldsManager).isRequired
+  };
+
   static propTypes = {
-    activity: PropTypes.object.isRequired
+    activity: PropTypes.object.isRequired,
   };
 
   constructor(props) {
@@ -27,11 +32,12 @@ export default class AFProjectCost extends Component {
     this.options = {
       withoutNoDataText: true
     };
+    this.updateData = this.updateData.bind(this);
   }
 
   getCurrencyCodeSelector(cell) {
     // TODO: return the list from possible value 'ppc_amount~currency_code' in a combo.
-    return (<span className={styles.editable}>{cell.value ? cell.value : cell}</span>);
+    return `<span class=${styles.editable}>${cell}</span>`; // Notice the `` for editable cell.
   }
 
   getDate(cell) {
@@ -40,6 +46,19 @@ export default class AFProjectCost extends Component {
 
   getAmount(cell) {
     return (<span className={styles.editable}>{NumberUtils.rawNumberToFormattedString(cell, true)}</span>);
+  }
+
+  getListOfCurrencies() {
+    // TODO: Check if this is the best way.
+    return Object.keys(this.context.activityFieldsManager._possibleValuesMap[FP.PPC_AMOUNT_CURRENCY_CODE_PATH]).sort();
+  }
+
+  numberValidator(value) {
+    const nan = isNaN(parseFloat(value, 10));
+    if (nan) {
+      return translate('Not a number');
+    }
+    return true;
   }
 
   generateProposedProjectCost() {
@@ -52,7 +71,7 @@ export default class AFProjectCost extends Component {
       this.props.activity[AC.PPC_AMOUNT].forEach((item) => (
         data.push({
           [AC.AMOUNT]: NumberUtils.rawNumberToFormattedString(item[AC.AMOUNT], true),
-          [AC.CURRENCY_CODE]: item[AC.CURRENCY_CODE],
+          [AC.CURRENCY_CODE]: item[AC.CURRENCY_CODE].value,
           [AC.FUNDING_DATE]: createFormattedDate(item[AC.FUNDING_DATE])
         })
       ));
@@ -60,17 +79,19 @@ export default class AFProjectCost extends Component {
       // TODO: check FM.
       if (true) {
         columns.push(<TableHeaderColumn
-          dataField={AC.AMOUNT} editable
+          dataField={AC.AMOUNT} editable={false}
           dataFormat={this.getAmount}>{translate('Amount')}</TableHeaderColumn>);
       }
       // TODO: check FM.
       if (true) {
         columns.push(<TableHeaderColumn
-          dataField={AC.CURRENCY_CODE} editable
+          dataField={AC.CURRENCY_CODE}
+          editable={{ type: 'select', options: { values: this.getListOfCurrencies() } }}
           dataFormat={this.getCurrencyCodeSelector}>{translate('Currency')}</TableHeaderColumn>);
       }
       // TODO: check FM.
       if (true) {
+        // TODO: Add a datepicker component.
         columns.push(<TableHeaderColumn
           dataField={AC.FUNDING_DATE} editable
           dataFormat={this.getDate}>{translate('Date')}</TableHeaderColumn>);
@@ -106,13 +127,14 @@ export default class AFProjectCost extends Component {
       // TODO: check FM.
       if (true) {
         columns.push(<TableHeaderColumn
-          dataField={AC.AMOUNT} editable
+          dataField={AC.AMOUNT} editable={{ validator: this.numberValidator }}
           dataFormat={this.getAmount}>{translate('Amount')}</TableHeaderColumn>);
       }
       // TODO: check FM.
       if (true) {
         columns.push(<TableHeaderColumn
-          dataField={AC.CURRENCY_CODE} editable
+          dataField={AC.CURRENCY_CODE}
+          editable={{ type: 'select', options: { values: this.getListOfCurrencies() } }}
           dataFormat={this.getCurrencyCodeSelector}>{translate('Currency')}</TableHeaderColumn>);
       }
       // TODO: check FM.
