f5a28921d8db1dbf59e2925d2d5140c0cfd6d12f
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-06-29 20:20:13 -0300

    AMPOFFLINE-434: Changes for lint.

--- app/components/activity/edit/components/AFList.jsx
+++ app/components/activity/edit/components/AFList.jsx
@@ -129,7 +129,7 @@ export default class AFList extends Component {
         <TableHeaderColumn
           key={childFieldName} dataField={childFieldName}
           editable={{ readOnly: !editable, validator }} columnClassName={this.editableCellClass.bind(this, editable)}
-	  dataFormat={this.columnFormatter.bind(null, editable)}
+          dataFormat={this.columnFormatter.bind(null, editable)}
         >
           {this.props.activityFieldsManager.getFieldLabelTranslation(fieldPath)}
         </TableHeaderColumn>);
@@ -145,10 +145,10 @@ export default class AFList extends Component {
     };
     // there is no one click row removal, we'll simulate with select
     return (<div>
-      <FormGroup controlId={`${this.props.listPath}-list`} validationState={this.validate()} >
+      <FormGroup controlId={`${this.props.listPath}-list`} validationState={this.validate()}>
         <BootstrapTable
           data={this.state.values} hover selectRow={selectRow} deleteRow options={this.options} cellEdit={cellEdit}
-          containerClass={styles.containerTable} tableHeaderClass={styles.header} thClassName={styles.thClassName} >
+          containerClass={styles.containerTable} tableHeaderClass={styles.header} thClassName={styles.thClassName}>
           {columns}
         </BootstrapTable>
         <FormControl.Feedback />
