7ce12257f393ca6859f35f87fc0a444b94d99b35
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-07-10 16:45:59 -0300

    AMPOFFLINE-424 make USD default currency upon first startup until sync

--- app/actions/CurrencyRatesAction.js
+++ app/actions/CurrencyRatesAction.js
@@ -4,6 +4,7 @@
 import CurrencyRatesHelper from '../modules/helpers/CurrencyRatesHelper';
 import GlobalSettingsHelper from '../modules/helpers/GlobalSettingsHelper';
 import LoggerManager from '../modules/util/LoggerManager';
+import { DEFAULT_CURRENCY } from '../utils/constants';
 import { BASE_CURRENCY_KEY } from '../utils/constants/GlobalSettingsConstants';
 
 export const STATE_LOADING_CURRENCY_RATES = 'STATE_LOADING_CURRENCY_RATES';
@@ -22,7 +23,14 @@ export function loadCurrencyRates() {
     dispatch(sendingRequest());
     return CurrencyRatesHelper.findAll({}).then((rates) =>
       GlobalSettingsHelper.findByKey(BASE_CURRENCY_KEY).then((gsBaseCurrency) => {
-        const baseCurrency = gsBaseCurrency.value;
+        // gsBaseCurrency is undefined it means that its the first time we run the app
+        // we will use the default currency USD
+        let baseCurrency;
+        if (gsBaseCurrency) {
+          baseCurrency = gsBaseCurrency.value;
+        } else {
+          baseCurrency = DEFAULT_CURRENCY;
+        }
         return dispatch(ratesLoaded(rates, baseCurrency));
       })
     ).catch(reject);
