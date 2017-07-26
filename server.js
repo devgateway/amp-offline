8a938bded475533dbcb30642f502da6f92365be3
Alexei <savca.alexei@gmail.com>
2017-07-26 20:51:02 +0300

    AMPOFFLINE-641 Removed unused import

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import StringReplacePlugin from 'string-replace-webpack-plugin';
 import { spawn } from 'child_process';
 
 import config from './webpack.config.development';
