cecf64290aee052b2b3f961b860eca038170c751
Alexei Savca <savca.alexei@gmail.com>
2017-07-27 13:17:48 +0300

    AMPOFFLINE-641 Using package.json dependencies as a base for dll

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -3,39 +3,49 @@ import path from 'path';
 import validate from 'webpack-validator';
 import merge from 'webpack-merge';
 import baseConfig from './webpack.config.base';
+import { dependencies } from './package.json';
+
+const BLACKLISTED_PKGS = [ // use this to exclude pkgs if they're causing you trouble
+  'font-awesome',
+  'babel-plugin-webpack-alias',
+  'electron-debug',
+  'electron-mocha'
+];
+
+const FORCE_PKGS = [ // use this to add pckgs that are not 1st level dependencies
+  'core-js',
+  'rc-calendar',
+  '@allenfang/react-toastr',
+  'react-bootstrap-table',
+  'antd/lib/locale-provider/en_US',
+  'antd/lib/locale-provider/fr_FR',
+  'antd/lib/locale-provider/bg_BG',
+  'antd/lib/locale-provider/ca_ES',
+  'antd/lib/locale-provider/zh_TW',
+  'antd/lib/locale-provider/cs_CZ',
+  'antd/lib/locale-provider/nl_NL',
+  'antd/lib/locale-provider/et_EE',
+  'antd/lib/locale-provider/fi_FI',
+  'antd/lib/locale-provider/de_DE',
+  'antd/lib/locale-provider/it_IT',
+  'antd/lib/locale-provider/ja_JP',
+  'antd/lib/locale-provider/ko_KR',
+  'antd/lib/locale-provider/pl_PL',
+  'antd/lib/locale-provider/pt_BR',
+  'antd/lib/locale-provider/ru_RU',
+  'antd/lib/locale-provider/sk_SK',
+  'antd/lib/locale-provider/es_ES',
+  'antd/lib/locale-provider/sv_SE',
+  'antd/lib/locale-provider/tr_TR',
+  'antd/lib/locale-provider/vi_VN',
+  'antd/lib/locale-provider/th_TH'
+];
 
 export default validate(merge(baseConfig, {
   entry: {
-    lib: [
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-      'react-router-redux',
-      'moment',
-      'antd',
-      'react-bootstrap',
-      'react-bootstrap-table',
-      'react-scrollspy',
-      'request',
-      'underscore',
-      'bluebird-queue',
-      'nedb',
-      'bluebird',
-      'crypto-js',
-      'jsonschema',
-      'text-encoding',
-      'bunyan',
-      'i18next',
-      'i18next-sync-fs-backend',
-      'redux',
-      'redux-thunk',
-      'redux-promise-middleware',
-      'redux-logger',
-      'moment',
-      'rc-menu',
-      'core-js'
-    ]
+    lib: Object.keys(dependencies)
+      .filter(dep => BLACKLISTED_PKGS.indexOf(dep) === -1)
+      .concat(FORCE_PKGS)
   },
 
   output: {
