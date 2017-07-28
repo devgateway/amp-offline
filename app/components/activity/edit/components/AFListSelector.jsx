b2a4a04bf4db73fdad9c0be0d1d9e42c1b5e1649
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-07-29 00:16:05 +0300

    AMPOFFLINE-601 display "Search <Field Name>" labels over the search

--- app/components/activity/edit/components/AFListSelector.jsx
+++ app/components/activity/edit/components/AFListSelector.jsx
@@ -4,13 +4,21 @@ import styles from './AFListSelector.css';
 import AFList from './AFList';
 import AFSearchList from './AFSearchList';
 import AFOption from './AFOption';
+import AFLabel from './AFLabel';
 import ActivityFieldsManager from '../../../../modules/activity/ActivityFieldsManager';
 import ActivityValidator from '../../../../modules/activity/ActivityValidator';
+import * as AC from '../../../../utils/constants/ActivityConstants';
 import translate from '../../../../utils/translate';
 import LoggerManager from '../../../../modules/util/LoggerManager';
 import * as Utils from '../../../../utils/Utils';
 
 /* eslint-disable class-methods-use-this */
+
+/** Explicit definition of the search field label parts (instead of automatic detection), e.g. when plural is used */
+const fieldNameToSearchFieldLabel = {};
+fieldNameToSearchFieldLabel[AC.ORGANIZATION] = 'Search Organizations';
+fieldNameToSearchFieldLabel[AC.LOCATION] = 'Search Locations';
+fieldNameToSearchFieldLabel[AC.PROGRAM] = 'Add Program';
 
 /**
  * Activity Form List options selection, with possibility to add percentage and additional element input text
@@ -47,6 +55,8 @@ export default class AFListSelector extends Component {
     this.listDef = this.context.activityFieldsManager.getFieldDef(this.props.listPath);
     // assumption based on current use cases is that we have only one id-only field to select
     this.idOnlyField = this.listDef.children.find(item => item.id_only === true).field_name;
+    this.searchLabel = fieldNameToSearchFieldLabel[this.idOnlyField]
+      || `Search ${AC.toOriginalLabel(this.idOnlyField)}`;
     this.percentageFieldDef = this.listDef.children.find(item => item.percentage === true);
     this.uniqueIdCol = this.uniqueConstraint || this.idOnlyField;
     this.setUniqueIdsAndUpdateState(this.props.selectedOptions || []);
@@ -128,6 +138,7 @@ export default class AFListSelector extends Component {
   render() {
     const searchDisplayStyle = this.noMultipleValues && this.state.values.length > 0 ? styles.hidden : styles.inline;
     const btnStyle = `${this.percentageFieldDef ? styles.dividePercentage : styles.hidden} btn btn-success`;
+
     return (<div >
       <AFList
         onDeleteRow={this.handleRemoveValue} values={this.getListValues()} listPath={this.props.listPath}
@@ -136,6 +147,9 @@ export default class AFListSelector extends Component {
         <FormControl.Feedback />
         <HelpBlock >{this.props.validationError}</HelpBlock >
       </FormGroup >
+      <div>
+        <AFLabel value={translate(this.searchLabel)} />
+      </div>
       <div className={`${searchDisplayStyle} ${styles.searchContainer}`} >
         <AFSearchList onSearchSelect={this.handleAddValue} options={this.props.options} />
         <Button
