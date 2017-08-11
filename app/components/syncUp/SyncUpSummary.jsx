28d5c7b71249747e1fc224e09d8fdb65062d53f6
Alexei Savca <savca.alexei@gmail.com>
2017-08-11 06:31:24 +0300

    AMPOFFLINE-470 First 3 summary entries

--- app/components/syncUp/SyncUpSummary.jsx
+++ app/components/syncUp/SyncUpSummary.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import translate from '../../utils/translate';
+import { createFormattedDateTime } from '../../utils/DateUtils';
 
-class SyncUpSummary extends React.Component {
+const entry = (label, value) => (
+  <div className="col-md-12">
+    <strong>{label}</strong>
+    &nbsp;
+    {value}
+  </div>
+);
+
+class SyncUpSummary extends PureComponent {
+  static propTypes = {
+    data: PropTypes.object,
+    getHistory: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    const { data, getHistory } = this.props;
+    if (!data) getHistory();
+  }
+
   render() {
+    const { data } = this.props;
+    if (!data) return null;
+    const { status, timestamp } = data;
     return (
-      <h1>Sync Up Summary</h1>
+      <div className="container">
+        {entry(translate('Status'), status)}
+        {entry(translate('Started'), createFormattedDateTime(timestamp))}
+        {entry(translate('Finished'), createFormattedDateTime(data['sync-date']))}
+      </div>
     );
   }
 }
--- app/containers/SyncUpSummaryPage.js
+++ app/containers/SyncUpSummaryPage.js
@@ -1,12 +1,19 @@
 import { connect } from 'react-redux';
 import SyncUpSummary from '../components/syncUp/SyncUpSummary';
+import { getSyncUpHistory } from '../actions/SyncUpAction';
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps(state) {
+  return {
+    data: state.syncUpReducer.historyData[0]
+  };
 }
 
-function mapDispatchToProps() {
-  return {};
+function mapDispatchToProps(dispatch) {
+  return {
+    getHistory: () => {
+      dispatch(getSyncUpHistory());
+    }
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SyncUpSummary);
