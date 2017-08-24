3c1887cf37ace71f2b72c4a84e7871e189074385
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-08-24 18:45:43 -0300

    AMPOFFLINE-676: Added the new funding item button with basic logic to add a new element.

--- app/components/activity/edit/sections/funding/AFFundingDonorSection.jsx
+++ app/components/activity/edit/sections/funding/AFFundingDonorSection.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component, PropTypes } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Panel, Button } from 'react-bootstrap';
 import * as AC from '../../../../../utils/constants/ActivityConstants';
 import LoggerManager from '../../../../../modules/util/LoggerManager';
 import ActivityFieldsManager from '../../../../../modules/activity/ActivityFieldsManager';
@@ -32,20 +32,61 @@ export default class AFFundingDonorSection extends Component {
     const openFundingsState = [];
     this._filterFundings(this.props.fundings).map(() => (openFundingsState.push(false)));
     this.state = {
-      openFundingDonorSection: openFundingsState
+      openFundingDonorSection: openFundingsState,
+      fundingList: this.props.fundings
     };
+    this._addNewFundingItem = this._addNewFundingItem.bind(this);
+    // console.log(this.props.fundings);
+  }
+
+  _addNewFundingItem() {
+    // TODO: Construir un nuevo funding con la misma estructura de los otros pero vacío, podría "clonar y limpiar" uno
+    // existente pero y si no tengo porque es el primero?
+    const fundingItem = {};
+    fundingItem.active = false;
+    fundingItem.active_list = [];
+    fundingItem.actual_completion_date = null;
+    fundingItem.actual_start_date = null;
+    fundingItem.agreement = null;
+    fundingItem.cal_type = null;
+    fundingItem.capital_spending_percentage = true;
+    fundingItem.comments = null;
+    fundingItem.conditions = null;
+    fundingItem.crs_transaction_number = null;
+    fundingItem.delegated_cooperation = null;
+    fundingItem.delegated_partned = null;
+    fundingItem.donor_objective = null;
+    fundingItem.donor_organization_id = {
+      extra_info: {
+        acronym: 'nada',
+        organization_group: 'nada'
+      },
+      id: 105,
+      'translated-value': { en: 'nada', fr: 'nada fr' },
+      value: 'nada'
+    };
+    fundingItem.financing_id = null;
+    fundingItem.financing_instrument = null;
+    fundingItem.funding_classification_date = null;
+    fundingItem.funding_details = [];
+    fundingItem.funding_status = null;
+    fundingItem.source_role = { id: 1, value: 'Donor' };
+    fundingItem.type_of_assistance = null;
+    const newFundingList = this.state.fundingList;
+    newFundingList.push(fundingItem);
+    this.setState({ fundingList: newFundingList });
   }
 
   _filterFundings(fundings) {
     return fundings.filter(f => (f[AC.FUNDING_DONOR_ORG_ID].id === this.props.organization.id
-    && f[AC.SOURCE_ROLE].id === this.props.role.id));
+      && f[AC.SOURCE_ROLE].id === this.props.role.id));
   }
 
   _generateComplexHeader(i, funding) {
-    // TODO: AFFields objects are refreshed.
+    // TODO: AFFields objects are not being refreshed.
     return (<div>
       <div>{`${translate('Funding Item')} ${i + 1}`}</div>
-      <div className={styles.header} >
+      <div className={styles.header}>
         <AFField
           fieldPath={`${AC.FUNDINGS}~${AC.TYPE_OF_ASSISTANCE}`} parent={funding}
           className={styles.header_small_item} showLabel={false} type={Types.LABEL} />
@@ -68,8 +109,8 @@ export default class AFFundingDonorSection extends Component {
 
   render() {
     // Filter only the fundings for this organization and role.
-    return (<div className={styles.container} >
-      {this._filterFundings(this.props.fundings).map((g, i) => (
+    return (<div className={styles.container}>
+      {this._filterFundings(this.state.fundingList).map((g, i) => (
         <Panel
           header={this._generateComplexHeader(i, g)}
           key={g[AC.GROUP_VERSIONED_FUNDING]} collapsible expanded={this.state.openFundingDonorSection[i]}
@@ -77,10 +118,11 @@ export default class AFFundingDonorSection extends Component {
             const newOpenState = this.state.openFundingDonorSection;
             newOpenState[i] = !newOpenState[i];
             this.setState({ openFundingDonorSection: newOpenState });
-          }} >
+          }}>
           <AFFundingContainer funding={g} />
         </Panel>
       ))}
+      <Button bsStyle="primary" onClick={this._addNewFundingItem}>{translate('New Funding Item')}</Button>
     </div>);
   }
 }
