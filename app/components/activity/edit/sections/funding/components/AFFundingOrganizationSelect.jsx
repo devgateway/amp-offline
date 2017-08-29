fec5da7510606267a7f211a3d2ff0133dc6bfc4e
Gabriel Inchauspe <ginchauspe@developmentgateway.org>
2017-08-29 11:52:19 -0300

    AMPOFFLINE-677: It adds a new donor (hardcoded the type for now), it doesnt update the tabs automatically yet.

--- app/components/activity/edit/sections/funding/components/AFFundingOrganizationSelect.jsx
+++ app/components/activity/edit/sections/funding/components/AFFundingOrganizationSelect.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component, PropTypes } from 'react';
-import * as AC from '../../../../../../utils/constants/ActivityConstants';
-import * as VC from '../../../../../../utils/constants/ValueConstants';
 import LoggerManager from '../../../../../../modules/util/LoggerManager';
-import translate from '../../../../../../utils/translate';
 import AFField from '../../../components/AFField';
+import * as AC from '../../../../../../utils/constants/ActivityConstants';
 
 /**
  * Funding Section
@@ -19,14 +17,39 @@ export default class AFFundingOrganizationSelect extends Component {
   constructor(props) {
     super(props);
     LoggerManager.log('constructor');
+    this.state = {
+      fundingList: this.props.activity.fundings
+    };
+  }
+
+  _handleDonorSelect(value) {
+    LoggerManager.log('_handleDonorSelect');
+    // TODO: ver si tengo q sacar el nuevo org de la tabla.
+    // TODO: insertar en this.context.activity o this.props.activity el nuevo funding.
+    const fundingItem = {};
+    fundingItem[AC.FUNDING_DONOR_ORG_ID] = {
+      id: value._id,
+      value: value._value,
+      extra_info: value.extra_info,
+      'translated-value': value['translated-value']
+    };
+    fundingItem[AC.SOURCE_ROLE] = { id: 1, value: 'Donor' };
+    fundingItem[AC.FUNDING_DETAILS] = [];
+    fundingItem[AC.GROUP_VERSIONED_FUNDING] = Math.random();
+    fundingItem[AC.AMP_FUNDING_ID] = Math.random();
+    const newFundingList = this.state.fundingList;
+    newFundingList.push(fundingItem);
+    this.setState({ fundingList: newFundingList });
   }
 
   render() {
     return (<div>
-      <div>{translate('Search Funding Organizations')}</div>
       <div>
-        <AFField parent={this.props.activity} fieldPath={'organization_group'} listParams={{ 'no-table': true }} />
-        <AFField parent={this.props.activity} fieldPath={AC.DONOR_ORGANIZATION} listParams={{ 'no-table': true }} />
+        Esto funca relativamente bien: deja lo elegido, devuelve ese solo y me lo agrega a la lista de donor
+        organizations.
+        <AFField
+          parent={this.props.activity} fieldPath={'donor_organization~organization'}
+          listParams={{ 'no-table': true }} onAfterUpdate={this._handleDonorSelect.bind(this)} />
       </div>
     </div>);
   }
