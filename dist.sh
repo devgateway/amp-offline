af74010026a420058d3defd1edd71d071e4a291e
Alexei Savca <savca.alexei@gmail.com>
2017-09-26 16:14:32 +0300

    AMPOFFLINE-714 Export PR_NR

--- dist.sh
+++ dist.sh
@@ -6,6 +6,7 @@ rm -r dist
 # Then create two Windows (NSIS) installers once at a time since doing both at the same time will result in one
 # installer supporting 32 and 64 bit architectures.
 # Last step changes ownership of the installers from root:root to user that launched this script.
+export PR_NR=$1
 DIST_CMD="
     npm run build &&
     npm run package-win-64 && rename 's/.exe/-64.exe/' dist/*.exe &&
@@ -14,7 +15,7 @@ DIST_CMD="
 docker run --rm -t -v ${PWD}:/project \
 	-v amp-client-electron:/root/.electron \
 	-v amp-client-cache:/root/.cache \
-	electronuserland/electron-builder:wine /bin/bash -c PR_NR=$1 "$DIST_CMD"
+	electronuserland/electron-builder:wine /bin/bash -c "$DIST_CMD"
 
 # Clean ~/.electron, execute only if really needed
 # docker volume rm amp-client-electron
