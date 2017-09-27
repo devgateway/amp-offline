9e7378041700b7f1b585b1eb0127a2a2d913042d
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-09-27 17:19:23 -0300

    AMPOFFLINE-665: Show Ministry Code only when Activity Budget is 'On Budget'.

--- app/utils/constants/ActivityConstants.js
+++ app/utils/constants/ActivityConstants.js
@@ -144,6 +144,7 @@ export const CAPITAL_SPENDING_PERCENTAGE = 'capital_spending_percentage';
 export const REPORTING_DATE = 'reporting_date';
 export const RECIPIENT_ROLE = 'recipient_role';
 export const TEMPORAL_ID = '_temporal_id';
+export const MINISTRY_CODE = 'ministry_code'
 
 // Activity labels (usually those that don't come as part of Fields Def EP)
 export const SAME_AS_PROPOSED_START_DATE_LABEL = 'Same as Proposed Start Date';
