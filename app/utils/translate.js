f45325074b821af071e848b5aa046c9fda41b15a
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-10-09 19:40:15 +0300

    AMPOFFLINE-144 document translation method

--- app/utils/translate.js
+++ app/utils/translate.js
@@ -1,7 +1,12 @@
 import i18next from 'i18next';
 import LoggerManager from '../modules/util/LoggerManager';
 
-export default (k, lng = undefined) => {
+/**
+ * Translates a message to the current language or to the specified one
+ * @param k the message to translate
+ * @param lng (optional) the language to which to translate. If missing, then the currently set language is used.
+ */
+export default (k, lng) => {
   let ret = i18next.t(k, { lng });
   if (ret === undefined) {
     ret = k;
