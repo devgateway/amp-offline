b640b3b357d0d452738e74f6969413399860e7af
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-10-10 00:48:28 -0300

    AMPOFFLINE-440: Links working.

--- app/components/login/LoginTroubleshootingLinks.jsx
+++ app/components/login/LoginTroubleshootingLinks.jsx
@@ -1,5 +1,5 @@
+/* eslint jsx-a11y/href-no-hash: 0 */
 import React, { Component, PropTypes } from 'react';
-import styles from './Login.css';
 import LoggerManager from '../../modules/util/LoggerManager';
 import translate from '../../utils/translate';
 
@@ -7,7 +7,7 @@ export default class LoginTroubleshootingLinks extends Component {
 
   static propTypes = {
     changePasswordOnline: PropTypes.func.isRequired,
-    solveLoginProblemsOnline: PropTypes.func.isRequired
+    resetPasswordOnline: PropTypes.func.isRequired
   };
 
   constructor() {
@@ -21,7 +21,7 @@ export default class LoginTroubleshootingLinks extends Component {
       <div>
         <a href="#" onClick={this.props.changePasswordOnline.bind(this)}>{translate('Change Password')}</a>
         <br />
-        <a href="#" onClick={this.props.solveLoginProblemsOnline.bind(this)}>{translate('Trouble signing in?')}</a>
+        <a href="#" onClick={this.props.resetPasswordOnline.bind(this)}>{translate('Trouble signing in?')}</a>
       </div>
     );
   }
