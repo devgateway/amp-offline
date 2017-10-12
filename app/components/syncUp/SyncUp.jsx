0b7d0ccf1874b6c59c10c62410a51e5692971d00
Alexei Savca <savca.alexei@gmail.com>
2017-10-12 13:29:27 +0300

    AMPOFFLINE-314 Sorting the sync logs before slicing

--- app/components/syncUp/SyncUp.jsx
+++ app/components/syncUp/SyncUp.jsx
@@ -232,8 +232,8 @@ export default connect(
       logoutDismissedToSync: state.loginReducer.logoutDismissedToSync,
       currentUserHistory: syncUpReducer.historyData
         .filter(datum => datum['requested-by'] === userReducer.userData.id)
-        .slice(0, NR_SYNC_HISTORY_ENTRIES)
         .sort((a, b) => new Date(a['sync-date']) - new Date(b['sync-date']))
+        .slice(0, NR_SYNC_HISTORY_ENTRIES)
     };
   },
 
