5a8bad4d7ba08cfb66ed2f2ccc03eb42717e33d9
Alexei <savca.alexei@gmail.com>
2017-10-24 16:47:55 +0300

    AMPOFFLINE-506 Missed a rename

--- app/actions/SyncUpAction.js
+++ app/actions/SyncUpAction.js
@@ -26,7 +26,7 @@ export const STATE_SYNCUP_LOG_LOADED = 'STATE_SYNCUP_LOG_LOADED';
 const logger = new Logger('Syncup action');
 
 export function loadSyncUpHistory() {
-  LoggerManager.log('getSyncUpHistory');
+  logger.log('getSyncUpHistory');
   return (dispatch) => {
     if (store.getState().syncUpReducer.loadingSyncHistory === false) {
       SyncUpManager.getSyncUpHistory().then((data) => (
@@ -50,7 +50,7 @@ export function startSyncUpIfConnectionAvailable() {
 }
 
 export function startSyncUp(historyData) {
-  LoggerManager.log('startSyncUp');
+  logger.log('startSyncUp');
   /* Save current syncup redux state because this might be a "forced" syncup and we dont want
    the user to be able to leave the page if this syncup fails. */
   if (store.getState().syncUpReducer.syncUpInProgress === false) {
@@ -61,7 +61,7 @@ export function startSyncUp(historyData) {
     return SyncUpManager.syncUpAllTypesOnDemand()
       .then((log) => {
         const newHistoryData = Object.assign({}, historyData, { status: SYNC_STATUS_COMPLETED });
-        LoggerManager.log('syncupSucessfull');
+        logger.log('syncupSucessfull');
         store.dispatch({ type: 'STATE_SYNCUP_COMPLETED', actionData: newHistoryData });
         const { id } = log;
         store.dispatch({
@@ -72,7 +72,7 @@ export function startSyncUp(historyData) {
         return checkIfToForceSyncUp();
       }
     ).catch((err) => {
-      LoggerManager.error(err);
+      logger.error(err);
       const errorMessage = err.errorCode === ERROR_CODE_NO_CONNECTIVITY ? err.message : translate('defaultSyncError');
       store.dispatch({ type: 'STATE_SYNCUP_FAILED', actionData: { errorMessage } });
       URLUtils.forwardTo('/syncUpSummary');
@@ -101,7 +101,7 @@ export function dismissSyncAndChooseWorkspace() {
 }
 
 function syncUpSearchHistoryOk(data) {
-  LoggerManager.log(`syncUpSearchHistoryOk: ${JSON.stringify(data)}`);
+  logger.log(`syncUpSearchHistoryOk: ${JSON.stringify(data)}`);
   return {
     type: STATE_SYNCUP_SHOW_HISTORY,
     actionData: data
@@ -109,7 +109,7 @@ function syncUpSearchHistoryOk(data) {
 }
 
 function syncUpSearchHistoryFailed(err) {
-  LoggerManager.log(`STATE_SYNCUP_SEARCH_FAILED: ${err}`);
+  logger.log(`STATE_SYNCUP_SEARCH_FAILED: ${err}`);
   return {
     type: STATE_SYNCUP_SEARCH_FAILED,
     actionData: { errorMessage: err }
@@ -117,14 +117,14 @@ function syncUpSearchHistoryFailed(err) {
 }
 
 function sendingRequest() {
-  LoggerManager.debug('sendingRequest');
+  logger.debug('sendingRequest');
   return {
     type: STATE_SYNCUP_LOADING_HISTORY
   };
 }
 
 function syncUpInProgress() {
-  LoggerManager.debug('sendingRequest');
+  logger.debug('sendingRequest');
   return {
     type: STATE_SYNCUP_IN_PROCESS
   };
