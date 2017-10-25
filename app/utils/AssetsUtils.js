c25d24d95b2df03e2890528a794e99eb2803fa94
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-10-26 00:19:13 +0300

    AMPOFFLINE-800 country flag placeholder

--- app/utils/AssetsUtils.js
+++ app/utils/AssetsUtils.js
@@ -7,7 +7,7 @@ import FileManager from '../modules/util/FileManager';
 class AssetsUtils {
   static loadImage(img) {
     // read binary data
-    if (FileManager.existsSync(img)) {
+    if (FileManager.existsSync(ASSEST_DIRECTORY, img)) {
       const bitmap = FileManager.readBinaryDataFileSync(ASSEST_DIRECTORY, img);
       return BASE_64_PNG_PREFIX + new Buffer(bitmap).toString('base64');
     } else {
