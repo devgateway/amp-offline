2488a761d8d86724eb3c4c8127587903ccaf3e50
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-11-02 11:57:31 +0200

    AMPOFFLINE-843 AF number value clearing handling

--- app/components/activity/edit/components/AFNumber.jsx
+++ app/components/activity/edit/components/AFNumber.jsx
@@ -49,9 +49,10 @@ export default class AFNumber extends Component {
   handleChange(e) {
     // TODO: I think keep in a variable the string representation of the number (according to current GS) along with
     // the numeric value.
-    const value = e.target.value;
+    let value = e.target.value;
+    value = value && value.trim();
     const validationError = this.validate(value);
-    const valueAsNumber = value === null || value === undefined ? value : Number(value);
+    const valueAsNumber = value === null || value === undefined || value === '' ? null : Number(value);
     this.props.onChange(valueAsNumber, null, validationError);
     this.setState({ value });
   }
