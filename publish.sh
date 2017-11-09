1871fb3a7dca53c393ec726fdbe2793dcedc6bb5
Nadejda Mandrescu <nmandrescu@developmentgateway.org>
2017-11-10 01:32:55 +0200

    AMPOFFLINE-694 enable all installers; add manual and automatic options

--- publish.sh
+++ publish.sh
@@ -7,5 +7,7 @@ fi
 
 BRANCH_NAME=$1
 
+INSTALLERS=`ls | grep -P "(exe|rpm|deb)"`
+
 ssh sulfur "mkdir -p /opt/amp-offline-snapshots/${BRANCH_NAME}"
-scp dist/*.exe sulfur:/opt/amp-offline-snapshots/${BRANCH_NAME}
+scp $INSTALLERS sulfur:/opt/amp-offline-snapshots/${BRANCH_NAME}
