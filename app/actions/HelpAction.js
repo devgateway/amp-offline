eb7b59e4dcfa5ba1992815ee2c493aadd605f656
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-11-17 12:26:46 -0300

    AMPOFFLINE-669: Cleanup action code.

--- app/actions/HelpAction.js
+++ app/actions/HelpAction.js
@@ -1,48 +1,32 @@
 import { ipcRenderer, remote } from 'electron';
 import os from 'os';
 import path from 'path';
-import url from 'url';
 import Logger from '../modules/util/LoggerManager';
 import FileManager from '../modules/util/FileManager';
 import Utils from '../utils/Utils';
-import { HELP_DIRECTORY, ASSETS_DIRECTORY, HELP_PDF_FILENAME, APP_DIRECTORY } from '../utils/Constants';
+import { HELP_PDF_FILENAME } from '../utils/Constants';
 
 export const STATE_OPEN_HELP_WINDOW = 'STATE_OPEN_HELP_WINDOW';
 
 const logger = new Logger('Help Action');
-const fs = remote.require('fs'); //TODO: si anda pasarlo a fs-extra.
-const { exec } = require('child_process');
+const fs = remote.require('fs');
 
 export function loadHelp() {
   logger.log('loadHelp');
   // We need to move/extract (depending if we run on prod or dev) the help pdf in order to open it.
   const to = path.join(os.tmpdir(), `${Utils.numberRandom()}-${HELP_PDF_FILENAME}`);
   const from = FileManager.getFullPathForBuiltInResources(HELP_PDF_FILENAME);
-  logger.log(from);
-  logger.log(to);
+  logger.debug(from);
+  logger.debug(to);
   /* Node 6.x doesnt have a function to copy files and fs-extra is not finding the original pdf inside the .asar, so
    we are using old fs functions, notice if we upgrade Node we might need to use fs.copyFileSync instead. */
   fs.writeFileSync(to, fs.readFileSync(from));
-  // fs.copyFileSync(from, to);
-  const fileLocation = url.format({
-    pathname: to,
-    protocol: 'file:',
-    slashes: false
-  });
-  // alert(fileLocation);
-  // ipcRenderer.send('createPDFWindow', fileLocation); // TODO: probar con abrir desde una location dentro del appData o mejor del .asar
-  const fixedLocation = url.format({
-    pathname: 'C:\\Users\\Gabriel\\AppData\\Local\\Programs\\amp-offline\\resources\\app.asar/amp-help.pdf',
-    protocol: 'file:',
-    slashes: false
-  });
-  ipcRenderer.send('createPDFWindow', encodeURIComponent('C:\\Users\\Gabriel\\AppData\\Local\\Programs\\amp-offline\\resources/app.asar/amp-help.pdf')); // esto funciona en dev!!!
-  // ipcRenderer.send('createPDFWindow', encodeURIComponent('C:\\Users\\Gabriel\\AppData\\Roaming\\AMPOffline\\amp-help.pdf')); // esto funciona en dev!!!
-  // ipcRenderer.send('createPDFWindow', 'C:\\Users\\Gabriel\\AppData\\Roaming\\AMPOffline\\amp-help.pdf'); // esto funciona en dev!!!
-  // ipcRenderer.send('createPDFWindow', encodeURIComponent('C:\\Users\\Gabriel\\AppData\\Roaming\\AMPOffline\\amp-help.pdf')); // esto funciona en dev!!!
-  //ipcRenderer.send('createPDFWindow', 'http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf'); // esto funciona en dev!!
-  //ipcRenderer.send('createPDFWindow', encodeURIComponent('http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf')); // esto funciona en dev!!
-  //exec(to);
+  ipcRenderer.send('createPDFWindow', encodeURIComponent(to));
+
+  /* An alternative option if rendering pdf inside chrome fails is to rely on the client's pdf reader:
+   * 1) const { exec } = require('child_process');
+   * 2) exec(to); */
+
   return (dispatch) => (
     dispatch({ type: STATE_OPEN_HELP_WINDOW })
   );
