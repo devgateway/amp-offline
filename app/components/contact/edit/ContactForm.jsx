91b57ca44cd96f555d6b66bdc5c3dd0901ea31fd
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-05-14 20:09:16 +0300

    AMPOFFLINE-458 basic initial contact form

--- app/components/contact/edit/ContactForm.jsx
+++ app/components/contact/edit/ContactForm.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import * as CC from '../../../utils/constants/ContactConstants';
+import FieldsManager from '../../../modules/field/FieldsManager';
+import EntityValidator from '../../../modules/field/EntityValidator';
 
 /**
  * Contact Form
@@ -6,12 +10,66 @@ import React, { Component } from 'react';
  * @author Nadejda Mandrescu
  */
 class ContactForm extends Component {
-  componentDidMount() {
-    // this.props.loadAllSummaryContacts();
+  static contextTypes = {
+    contactReducer: PropTypes.object.isRequired,
+  };
+
+  static propTypes = {
+    contactId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  };
+
+  static childContextTypes = {
+    contactReducer: PropTypes.object,
+    activity: PropTypes.object,
+    activityFieldsManager: PropTypes.instanceOf(FieldsManager),
+    activityValidator: PropTypes.instanceOf(EntityValidator),
+    isSaveAndSubmit: PropTypes.bool,
+  };
+
+  constructor(props) {
+    super(props);
+    // TODO new contact
+    this.state = {
+      contact: null
+    };
+  }
+
+  getChildContext() {
+    return {
+      ...this.context,
+      activityFieldsManager: this.context.contactReducer.contactFieldsManager,
+      activityValidator: this.contactValidator,
+    };
+  }
+
+  componentWillMount() {
+    const contact = this.context.contactReducer.contactsByIds[this.props.contactId];
+    this.init(this.context, contact);
+    this.setState({ contact });
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    this.init(nextContext);
+  }
+
+  init(context, contact = this.state.contact) {
+    const { contactFieldsManager } = context.contactReducer;
+    if (contactFieldsManager && !this.contactValidator) {
+      this.contactValidator = new EntityValidator(contact, contactFieldsManager, []);
+    }
   }
 
   render() {
-    return <div>TODO Contact Form</div>;
+    const { contact } = this.state;
+    if (!contact || !this.contactValidator) {
+      return null;
+    }
+    return (
+      <div>
+        <div>{`${contact[CC.NAME]} ${contact[CC.LAST_NAME]}`}</div>
+        <div>TODO Contact Form</div>
+      </div>
+    );
   }
 }
 
