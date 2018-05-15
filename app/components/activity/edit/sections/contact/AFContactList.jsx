0662c498b396b96fed0b9447eefc668d309b230a
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-05-15 12:23:52 +0300

    AMPOFFLINE-458 clear existing primary contact if another one is picked

--- app/components/activity/edit/sections/contact/AFContactList.jsx
+++ app/components/activity/edit/sections/contact/AFContactList.jsx
@@ -37,30 +37,50 @@ export default class AFContactList extends Component {
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({ values: nextProps.values });
+  }
+
   handleRemove(id) {
     this.props.onDeleteRow(id);
     const values = this.state.values.filter(v => v.id !== id);
     this.setState({ values });
   }
 
-  toContactItem(contactOption) {
-    const { listPath, onEditRow } = this.props;
-    const onItemEdit = (col) => onEditRow(contactOption, col, contactOption[col]);
-    const contactId = contactOption[AC.CONTACT].id;
-    const pcFieldPath = `${listPath}~${AC.PRIMARY_CONTACT}`;
+  changePrimary(contactRow, wasPrimary, isNowPrimary) {
+    if (isNowPrimary === wasPrimary) {
+      return;
+    }
+    if (isNowPrimary) {
+      const existingPrimary = this.state.values.find(v => v !== contactRow && v[AC.PRIMARY_CONTACT]);
+      if (existingPrimary) {
+        existingPrimary[AC.PRIMARY_CONTACT] = false;
+        this.handleEdit(existingPrimary, AC.PRIMARY_CONTACT);
+      }
+    }
+    this.handleEdit(contactRow, AC.PRIMARY_CONTACT);
+  }
+
+  handleEdit(row, fieldName) {
+    this.props.onEditRow(row, fieldName, row[fieldName]);
+  }
+
+  toContactItem(contactRow) {
+    const contactId = contactRow[AC.CONTACT].id;
     // TODO styling
     return (
-      <Row key={contactOption.uniqueId}>
+      <Row key={contactRow.uniqueId}>
         <Col md={7} lg={7} >
-          <ContactFormPage contactId={contactId} onEdit={() => onItemEdit(AC.CONTACT)} />
+          <ContactFormPage contactId={contactId} onEdit={() => this.handleEdit(contactRow, AC.CONTACT)} />
         </Col>
         <Col md={1} lg={1} >
           <AFField
-            parent={contactOption} fieldPath={pcFieldPath} onAfterUpdate={() => onItemEdit(AC.PRIMARY_CONTACT)} />
+            parent={contactRow} fieldPath={`${this.props.listPath}~${AC.PRIMARY_CONTACT}`}
+            onAfterUpdate={this.changePrimary.bind(this, contactRow, contactRow[AC.PRIMARY_CONTACT])} />
         </Col>
         <Col md={1} lg={1}>
           <a
-            onClick={this.handleRemove.bind(contactOption.uniqueId)} className={styles.delete} href={null}>
+            onClick={this.handleRemove.bind(contactRow.uniqueId)} className={styles.delete} href={null}>
             <span>&nbsp;</span>
           </a>
         </Col>
