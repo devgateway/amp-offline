6b69cd56655903763ceab926615356a8489dc47c
Gabriel Inchauspe <ginchauspe@gmail.com>
2018-07-02 16:45:41 -0300

    AMPOFFLINE-1098: Show structures without table. Fix number readonly option.

--- app/components/activity/edit/sections/AFStructures.jsx
+++ app/components/activity/edit/sections/AFStructures.jsx
@@ -7,9 +7,11 @@ import { STRUCTURES } from './AFSectionConstants';
 import * as AC from '../../../../utils/constants/ActivityConstants';
 import Logger from '../../../../modules/util/LoggerManager';
 import afStyles from '../ActivityForm.css';
+import * as Types from '../components/AFComponentTypes';
 import * as FMC from '../../../../utils/constants/FeatureManagerConstants';
 import * as FPC from '../../../../utils/constants/FieldPathConstants';
 import { ACTIVITY_ORGANIZATIONS_DONOR_ORGANIZATION } from '../../../../utils/constants/FeatureManagerConstants';
+import FieldsManager from '../../../../modules/field/FieldsManager';
 
 const logger = new Logger('AF Structures');
 
@@ -19,27 +21,59 @@ const logger = new Logger('AF Structures');
  */
 class AFStructures extends Component {
 
-  static propTypes = {
+  static contextTypes = {
     activity: PropTypes.object.isRequired
   };
+
+  static propTypes = {};
 
   constructor(props) {
     super(props);
     logger.log('constructor');
-    this.setState({ validationError: undefined });
+  }
+
+  preProcessForIds() {
+    const { activity } = this.context;
+    if (activity[AC.STRUCTURES]) {
+      activity[AC.STRUCTURES].forEach(s => {
+        // Maybe we will change the EP to send the id, anyway we need an id field for the AFSelector.
+        if (!s.id) {
+          s.id = Math.random();
+        }
+        /* TODO: Check if we need to remove this old field from the API because it breaks our component
+        ("type" is null but reported as id_only: true). */
+        delete s.type; // en lugar de borrar crearle un {id: random}
+      });
+    }
+  }
+
+  generateTable(structure) {
+    const content = [];
+    content.push(<Col md={3} lg={3}>
+      <AFField fieldPath={`${AC.STRUCTURES}~${AC.STRUCTURES_TITLE}`} parent={structure} type={Types.TEXT_AREA} />
+    </Col>);
+    content.push(<Col md={3} lg={3}>
+      <AFField fieldPath={`${AC.STRUCTURES}~${AC.STRUCTURES_DESCRIPTION}`} parent={structure} type={Types.TEXT_AREA} />
+    </Col>);
+    content.push(<Col md={3} lg={3}>
+      <AFField
+        fieldPath={`${AC.STRUCTURES}~${AC.STRUCTURES_LATITUDE}`} parent={structure} type={Types.NUMBER}
+        extraParams={{ readonly: true }} />
+    </Col>);
+    content.push(<Col md={3} lg={3}>
+      <AFField
+        fieldPath={`${AC.STRUCTURES}~${AC.STRUCTURES_LONGITUDE}`} parent={structure} type={Types.NUMBER}
+        extraParams={{ readonly: true }} />
+    </Col>);
+    return content;
   }
 
   render() {
-    const { activity } = this.props;
+    this.preProcessForIds();
+    const { activity } = this.context;
     return (<div className={afStyles.full_width}>
       <Grid className={afStyles.full_width}>
-        <Row>
-          <Col md={12} lg={12}>
-            <AFField
-              parent={activity}
-              fieldPath={AC.STRUCTURES} />
-          </Col>
-        </Row>
+        {activity[AC.STRUCTURES].map(s => (<Row>{this.generateTable(s)}</Row>))}
       </Grid>
     </div>);
   }
