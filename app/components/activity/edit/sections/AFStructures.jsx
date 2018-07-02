06c7bedfeeccb5e7beee356c597672c2bb8fdc5a
Gabriel Inchauspe <ginchauspe@gmail.com>
2018-07-02 17:37:23 -0300

    AMPOFFLINE-1098: Cleanup.

--- app/components/activity/edit/sections/AFStructures.jsx
+++ app/components/activity/edit/sections/AFStructures.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Col, FormControl, FormGroup, Grid, HelpBlock, Row } from 'react-bootstrap';
+import { Col, Grid, Panel, Row } from 'react-bootstrap';
 import AFSection from './AFSection';
 import AFField from '../components/AFField';
 import { STRUCTURES } from './AFSectionConstants';
@@ -8,10 +8,7 @@ import * as AC from '../../../../utils/constants/ActivityConstants';
 import Logger from '../../../../modules/util/LoggerManager';
 import afStyles from '../ActivityForm.css';
 import * as Types from '../components/AFComponentTypes';
-import * as FMC from '../../../../utils/constants/FeatureManagerConstants';
-import * as FPC from '../../../../utils/constants/FieldPathConstants';
-import { ACTIVITY_ORGANIZATIONS_DONOR_ORGANIZATION } from '../../../../utils/constants/FeatureManagerConstants';
-import FieldsManager from '../../../../modules/field/FieldsManager';
+import translate from '../../../../utils/translate';
 
 const logger = new Logger('AF Structures');
 
@@ -27,27 +24,7 @@ class AFStructures extends Component {
 
   static propTypes = {};
 
-  constructor(props) {
-    super(props);
-    logger.log('constructor');
-  }
-
-  preProcessForIds() {
-    const { activity } = this.context;
-    if (activity[AC.STRUCTURES]) {
-      activity[AC.STRUCTURES].forEach(s => {
-        // Maybe we will change the EP to send the id, anyway we need an id field for the AFSelector.
-        if (!s.id) {
-          s.id = Math.random();
-        }
-        /* TODO: Check if we need to remove this old field from the API because it breaks our component
-        ("type" is null but reported as id_only: true). */
-        delete s.type; // en lugar de borrar crearle un {id: random}
-      });
-    }
-  }
-
-  generateTable(structure) {
+  static generateTable(structure) {
     const content = [];
     content.push(<Col md={3} lg={3}>
       <AFField fieldPath={`${AC.STRUCTURES}~${AC.STRUCTURES_TITLE}`} parent={structure} type={Types.TEXT_AREA} />
@@ -68,12 +45,29 @@ class AFStructures extends Component {
     return content;
   }
 
+  constructor(props) {
+    super(props);
+    logger.log('constructor');
+  }
+
+  preProcessForIds() {
+    const { activity } = this.context;
+    if (activity[AC.STRUCTURES]) {
+      activity[AC.STRUCTURES].forEach(s => {
+        if (!s.id) {
+          s.id = Math.random();
+        }
+      });
+    }
+  }
+
   render() {
     this.preProcessForIds();
     const { activity } = this.context;
     return (<div className={afStyles.full_width}>
       <Grid className={afStyles.full_width}>
-        {activity[AC.STRUCTURES].map(s => (<Row>{this.generateTable(s)}</Row>))}
+        {activity[AC.STRUCTURES].map(s => (
+          <Row><Panel header={translate('Structure')}>{this.generateTable(s)}</Panel></Row>))}
       </Grid>
     </div>);
   }
