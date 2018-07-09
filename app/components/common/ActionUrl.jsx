c91f6f3235eb963529d2b0bdf5a6a96a25f92b7f
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-07-09 17:06:34 +0300

    AMPOFFLINE-1016 common ActionUrl & ActionIcon components

---
+++ app/components/common/ActionUrl.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { SHELL } from '../../modules/util/ElectronApp';
+import * as styles from './CommonStyles.css';
+
+/**
+ * Action Url
+ *
+ * @author Nadejda Mandrescu
+ */
+export default class ActionUrl extends Component {
+  static propTypes = {
+    urlContent: PropTypes.any.isRequired,
+    href: PropTypes.string, // external URL
+    navUrl: PropTypes.string, // navigation link
+    onClick: PropTypes.func, // a custom onClick action
+  };
+
+  renderExternalLink() {
+    const { href, urlContent } = this.props;
+    return <a className={styles.url} onClick={() => SHELL.openExternal(href)}>{urlContent}</a>;
+  }
+
+  renderCustomAction() {
+    const { onClick, urlContent } = this.props;
+    return <span onClick={onClick}>{urlContent}</span>;
+  }
+
+  renderNavigationLink() {
+    const { navUrl, urlContent } = this.props;
+    return <Link to={navUrl}>{urlContent}</Link>;
+  }
+
+  renderNoAction() {
+    const { urlContent } = this.props;
+    return <span className={styles.noAction}>{urlContent}</span>;
+  }
+
+  render() {
+    const { href, onClick, navUrl } = this.props;
+    if (href) {
+      return this.renderExternalLink();
+    }
+    if (onClick) {
+      return this.renderCustomAction();
+    }
+    if (navUrl) {
+      return this.renderNavigationLink();
+    }
+    return this.renderNoAction();
+  }
+}
--- app/modules/util/ElectronApp.js
+++ app/modules/util/ElectronApp.js
@@ -1,4 +1,4 @@
-const { app, remote } = require('electron');
+const { app, remote, shell } = require('electron');
 
 /** electron "app" instance in either main or remote rendering process */
 export const ELECTRON_APP = app || (remote && remote.app);
@@ -6,3 +6,5 @@ export const ELECTRON_APP = app || (remote && remote.app);
 export const IS_RENDERER_PROCESS = process && process.type === 'renderer';
 /** Tells if the app is running in development mode. */
 export const IS_DEV_MODE = process.env.NODE_ENV === 'development';
+/** electron "shell" that provides functions related to desktop integration */
+export const SHELL = shell || (remote && remote.shell);
