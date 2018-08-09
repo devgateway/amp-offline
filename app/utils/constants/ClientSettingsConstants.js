1077056942e2b05cbc9d6054a30cb90158e046da
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-08-09 10:33:23 +0300

    AMPOFFLINE-777 detecting AMP URL changes provided by AMP Registry

--- app/utils/constants/ClientSettingsConstants.js
+++ app/utils/constants/ClientSettingsConstants.js
@@ -6,6 +6,8 @@
 export const UPDATE_INSTALLER_PATH = 'Update Installer Path';
 export const LAST_CONNECTIVITY_STATUS = 'Last Connectivity Status';
 export const SETUP_CONFIG = 'Setup Config';
+export const AMP_SETTINGS_FROM_AMP_REGISTRY = 'AMP Settings from AMP Registry';
+export const LAST_AMP_SETTINGS_STATUS = 'Last AMP Settings status';
 export const DEFAULT_SETUP_COMPLETE = 'Default Setup Complete';
 export const AMP_SERVER_ID = 'AMP Server Id';
 
@@ -13,3 +15,6 @@ export const SETTING_TYPE_BOOLEAN = 'boolean';
 export const SETTING_TYPE_STRING = 'string';
 export const SETTING_TYPE_OBJECT = 'object';
 export const SETTING_TYPE_INTEGER = 'integer';
+
+export const LAST_AMP_SETTINGS_STATUS_PENDING = 'PENDING';
+export const LAST_AMP_SETTINGS_STATUS_REVIEWED = 'REVIEWED';
