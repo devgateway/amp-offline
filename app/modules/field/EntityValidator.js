a0e8f30cdf869772e7fda6e6dbb691b63765aa2a
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-08-13 19:39:48 +0300

    AMPOFFLINE-1086 use GS format for numbers; fix numeral.js validation

--- app/modules/field/EntityValidator.js
+++ app/modules/field/EntityValidator.js
@@ -203,11 +203,10 @@ export default class EntityValidator {
   }
 
   _initGenericErrors() {
-    const gsNumberFormat = GlobalSettingsManager.getSettingByKey(GS_DEFAULT_NUMBER_FORMAT);
     const gsDateFormat = GlobalSettingsManager.getSettingByKey(DEFAULT_DATE_FORMAT);
     this.invalidValueError = translate('invalidValue');
     this.invalidString = translate('invalidString');
-    this.invalidNumber = translate('invalidNumber').replace('%gs-format%', gsNumberFormat);
+    this.invalidNumber = translate('invalidNumber2');
     this.invalidBoolean = translate('invalidBoolean');
     this.invalidTitle = translate('duplicateTitle');
     // though we'll validate internal format, we have to report user friendly format
@@ -283,13 +282,13 @@ export default class EntityValidator {
         }
       } else if (fieldDef.field_type === 'long') {
         if (!Number.isInteger(value) && !this._isAllowInvalidNumber(value, fieldPath)) {
-          this.processValidationResult(obj, fieldPath, this.invalidNumber.replace('%value%', value));
+          this.processValidationResult(obj, fieldPath, this.invalidNumber);
         } else {
           this._wasValidatedSeparately(obj, fieldPath, fieldDef, asDraft);
         }
       } else if (fieldDef.field_type === 'float') {
         if (value !== +value || value.toString().indexOf('e') > -1) {
-          this.processValidationResult(obj, fieldPath, this.invalidNumber.replace('%value%', value));
+          this.processValidationResult(obj, fieldPath, this.invalidNumber);
         }
       } else if (fieldDef.field_type === 'boolean') {
         if (!(typeof value === 'boolean' || value instanceof Boolean)) {
