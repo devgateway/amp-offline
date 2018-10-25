087d47454ac6ff0f117be011668e24d94364b9c4
Gabriel Inchauspe <ginchauspe@gmail.com>
2018-10-25 03:25:17 +0300

    AMPOFFLINE-1154: Working fine in prod.

--- app/main.development.js
+++ app/main.development.js
@@ -72,10 +72,14 @@ app.on('ready', async () => {
     height: 325,
     transparent: false,
     frame: false,
-    alwaysOnTop: true,
+    titleBarStyle: 'hidden',
+    alwaysOnTop: false,
     resizable: false,
     show: false,
-    delay: 0
+    delay: 100,
+    backgroundColor: '#2b669a',
+    movable: false,
+    closable: false
   });
   splash.loadURL(`file://${__dirname}/splash-screen.html`);
   splash.once('ready-to-show', () => {
@@ -112,13 +116,9 @@ app.on('ready', async () => {
     closeApp();
   });
 
-  // if main window is ready to show, then destroy the splash window and show up the main window
-  mainWindow.once('ready-to-show', () => {
-    splash.destroy();
-    mainWindow.show();
-  });
-
   mainWindow.webContents.on('did-finish-load', () => {
+    splash.hide();
+    splash.destroy();
     mainWindow.show();
     mainWindow.focus();
   });
