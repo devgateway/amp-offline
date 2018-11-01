9c3491d66ae2a4c8c8bdeb1df56dc9d9d3df00c2
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-11-01 14:21:21 +0200

    AMPOFFLINE-1302 update temporary testing changelog per schema & tests

--- app/static/db/changelog/changelog-1.4.0.js
+++ app/static/db/changelog/changelog-1.4.0.js
@@ -2,17 +2,45 @@ import ChangelogLogger from '../ChangelogLogger';
 
 export default({
   changelog: {
-    preConditions: [{}],
+    preConditions: [
+      {
+        func: () => { ChangelogLogger.log('This is a valid precondition'); },
+        onError: 'HALT'
+      },
+    ],
     changesets: [
       {
         changeid: 'AMPOFFLINE-1301 successful',
         author: 'nmandrescu',
         comment: 'POC test 1',
+        preConditions: [
+          {
+            func: () => { ChangelogLogger.log('This is a valid precondition'); },
+            onError: 'HALT'
+          },
+          {
+            changeid: 'a valid changeset reference',
+            author: 'anyone',
+            file: 'some file',
+            onError: 'CONTINUE',
+            onFail: 'HALT'
+          },
+          {
+            changeid: 'a valid changeset reference',
+            author: 'anyone',
+            file: 'some file',
+            onFail: 'HALT'
+          }
+        ],
         changes: {
           func: () => { ChangelogLogger.log('This changeset must succeed'); },
         },
-        rollback: () => { ChangelogLogger.error('This rollback must NOT be called'); },
-        getMd5: () => 'e6c7af4edcef0f0b10d1a112b6a4c2ec'
+        rollback: {
+          func: () => {
+            ChangelogLogger.error('This rollback must NOT be called');
+          },
+        },
+        getMd5: () => 'aab3998f6ae5f41a43992f98197a5e6c'
       },
       {
         changeid: 'AMPOFFLINE-1301 unsuccessful',
@@ -21,8 +49,10 @@ export default({
         changes: {
           func: () => { ChangelogLogger.log('This changeset must fail'); return Promise.reject(); }
         },
-        rollback: () => { ChangelogLogger.log('This rollback must be called'); },
-        getMd5: () => '5bf5cad2419523f2788f9c1bc73e5a52'
+        rollback: {
+          func: () => { ChangelogLogger.log('This rollback must be called'); }
+        },
+        getMd5: () => '303e5724f56dd92f6ea85232bdc4bbf0'
       }
     ]
   },
