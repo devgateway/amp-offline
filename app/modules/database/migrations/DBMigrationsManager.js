c3da4fb5e94e55fec73e3b9442f8d1960e173295
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-11-07 19:42:58 +0200

    AMPOFFLINE-1303 detect pending change sets/logs, save new, log progress

--- app/modules/database/migrations/DBMigrationsManager.js
+++ app/modules/database/migrations/DBMigrationsManager.js
@@ -5,6 +5,9 @@ import changelogs from '../../../static/db/changelog-master';
 import * as MC from '../../../utils/constants/MigrationsConstants';
 import Changeset from './Changeset';
 import ChangelogSchema from './schema/ChangelogSchema';
+import ChangesetHelper from '../../helpers/ChangesetHelper';
+import * as Utils from '../../../utils/Utils';
+import DateUtils from '../../../utils/DateUtils';
 
 const logger = new Logger('DB Migrations Manager');
 
@@ -22,20 +25,94 @@ class DBMigrationsManager {
     return validator.validate(changelog, ChangelogSchema);
   }
 
+  constructor() {
+    this._executedChangesetIds = new Set();
+    this._pendingChangesetsById = new Map();
+    this._pendingChangelgs = undefined;
+    this._deployemntId = undefined;
+  }
+
+  /**
+   * Provides all currently pending changelogs
+   * @return {Promise}
+   */
   detectAndValidateChangelogs() {
-    // TODO full solution, for now quick POC implementation
-    return changelogs.filter(chdef => {
-      logger.debug(`Verifying '${chdef[MC.FILE]}' changelog`);
-      const changelog = chdef[MC.CONTENT];
-      const result = DBMigrationsManager.validateChangelog(changelog);
-      if (!result.valid) {
-        logger.error(`Skipping '${chdef[MC.FILE]}', since not in a valid format: ${JSON.stringify(result.errors)}`);
-        return false;
+    if (this._pendingChangelgs) {
+      return this._refreshPendingChangelogs();
+    }
+    return this._detectAllValidAndPendingChangelogs();
+  }
+
+  _refreshPendingChangelogs() {
+    const files = new Set(this._pendingChangesetsById.values().map((c:Changeset) => c.filename));
+    this._pendingChangelgs = this._pendingChangelgs.filter(cl => files.has(cl[MC.FILE]));
+    return Promise.resolve().then(() => this._pendingChangelgs);
+  }
+
+  _detectAllValidAndPendingChangelogs() {
+    return this._getChangesetsSummary().then((dbChangesetsMap:Map) => {
+      const newChangesets = [];
+      this._pendingChangelgs = changelogs.filter(chdef => {
+        const file = chdef[MC.FILE];
+        logger.debug(`Verifying '${file}' changelog`);
+        const changelog = chdef[MC.CONTENT];
+        const result = DBMigrationsManager.validateChangelog(changelog);
+        if (!result.valid) {
+          logger.error(`Skipping '${file}', since not in a valid format: ${JSON.stringify(result.errors)}`);
+          return false;
+        }
+        const pendingCount = this._detectPending(chdef, dbChangesetsMap, newChangesets);
+        logger.log(`Detected '${file}' changelog. Has ${pendingCount} pending changesets.`);
+        return pendingCount;
+      });
+      logger.info(`Found ${this._pendingChangesetsById.size} total changesets to execute.`);
+      return this._saveNewChangesets(newChangesets);
+    }).then(() => this._pendingChangelgs);
+  }
+
+  _getChangesetsSummary() {
+    return ChangesetHelper.findAllChangesets({}, { id: 1, [MC.MD5SUM]: 1, [MC.DEPLOYMENT_ID]: 1 })
+      .then(chs => {
+        if (this._deployemntId === undefined) {
+          this._deployemntId = Math.max(0, ...chs.map(c => c[MC.DEPLOYMENT_ID])) + 1;
+        }
+        return chs;
+      })
+      .then(Utils.toMapByKey);
+  }
+
+  _detectPending(chdef, dbChangesetsMap: Map, newChangesets: Array) {
+    const chs = chdef[MC.CONTENT][MC.CHANGELOG][MC.CHANGESETS];
+    return chs.filter(c => {
+      const changeset = new Changeset(c, chdef);
+      const dbC = dbChangesetsMap.get(changeset.id);
+      let willRun = changeset.isRunAlways || !dbC;
+      if (!dbC) {
+        newChangesets.push(changeset);
+      } else if (changeset.md5 !== c[MC.MD5SUM]) {
+        willRun = willRun || changeset.isRunOnChange;
+        logger.error(`${changeset.id}: MD5 mismatch detected. ${willRun ? 'It is' : 'Not'} scheduled to rerun.`);
       }
-      // TODO check if any changeset still needs to be executed and report those as detected
-      logger.log(`Detected '${chdef[MC.FILE]}' changelog to execute`);
-      return true;
-    });
+      if (willRun) {
+        this._pendingChangesetsById.set(changeset.id, changeset);
+        return true;
+      }
+      return false;
+    }).length;
+  }
+
+  _saveNewChangesets(newChangesets: Array) {
+    if (!newChangesets.length) {
+      return Promise.resolve();
+    }
+    const now = DateUtils.getISODateForAPI();
+    const template = {
+      [MC.DEPLOYMENT_ID]: this._deployemntId,
+      [MC.DATE_FOUND]: now,
+      [MC.DATE_EXECUTED]: now,
+    };
+    const dbcs = newChangesets.map(c => ChangesetHelper.changesetToDBFormat(c, template));
+    return ChangesetHelper.saveOrUpdateChangesetCollection(dbcs);
   }
 
   run(context: string) {
@@ -48,7 +125,7 @@ class DBMigrationsManager {
       const changelog = chdef[MC.CONTENT][MC.CHANGELOG];
       return prevPromise.then(() => {
         // TODO the full solution
-        logger.log(`Executing '${chdef[MC.FILE]}' changelog...`);
+        logger.log(`Checking '${chdef[MC.FILE]}' changelog...`);
         return this._checkPreConditions(changelog[MC.PRECONDITIONS]);
       }).then(preconditionsPass => {
         if (!preconditionsPass) {
