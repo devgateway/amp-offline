5a6b41adf6c090518e985e390ad6158db6baf4aa
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-11-20 19:35:05 +0200

    AMPOFFLINE-1307 generic changelog/set template

---
+++ test/modules/migrations/templates/generic-changeset.js
@@ -0,0 +1,14 @@
+import * as Utils from '../../../../app/utils/Utils';
+
+export const changeset = () => ({
+  changeid: `AMPOFFLINE-1307-${Utils.stringToUniqueId()}`,
+  author: 'nmandrescu',
+  comment: 'Generic changeset',
+  context: 'startup',
+  changes: {
+    func: () => {
+    }
+  }
+});
+
+export const changesets = () => ([changeset()]);
