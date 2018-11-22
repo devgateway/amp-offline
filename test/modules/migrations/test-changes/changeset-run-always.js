12763f23670dd91de407f92d0d45079af3270787
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-11-22 13:04:24 +0200

    AMPOFFLINE-1307 on run always test md5 and deployement id values are ok

--- test/modules/migrations/test-changes/changeset-run-always.js
+++ test/modules/migrations/test-changes/changeset-run-always.js
@@ -1,8 +1,9 @@
 import * as generic from '../templates/generic-changeset';
-import { checkExecutedCount, execTypeMatch, execTypeMatchAll, getChangesetId } from '../MigrationsTestUtils';
+import { checkExecutedCount, execTypeMatchAll, getChangesetId, matchByTemplate } from '../MigrationsTestUtils';
 import * as MC from '../../../../app/utils/constants/MigrationsConstants';
 import DBMigrationsManager from '../../../../app/modules/database/migrations/DBMigrationsManager';
 import FileManager from '../../../../app/modules/util/FileManager';
+import Changeset from '../../../../app/modules/database/migrations/Changeset';
 
 const fileName = FileManager.basename(__filename);
 
@@ -21,6 +22,19 @@ export default ({ changelog });
 const c1 = changelog.changesets[0];
 const c2 = changelog.changesets[1];
 
+const t1 = {
+  [MC.EXECTYPE]: MC.EXECTYPE_RERUN,
+  [MC.MD5SUM]: (cObj: Changeset) => (newMd5) => cObj.md5 === cObj.prevDBData[MC.MD5SUM] && cObj.md5 === newMd5,
+  [MC.DEPLOYMENT_ID]: (cObj: Changeset) => (newDId) => cObj.prevDBData[MC.DEPLOYMENT_ID] + 1 === newDId
+};
+
+const t2 = {
+  [MC.EXECTYPE]: MC.EXECTYPE_EXECUTED,
+  [MC.MD5SUM]: (cObj: Changeset) => (newMd5) => cObj.md5 === cObj.prevDBData[MC.MD5SUM] && cObj.md5 === newMd5,
+  [MC.DEPLOYMENT_ID]: (cObj: Changeset) => (newDId) => cObj.prevDBData[MC.DEPLOYMENT_ID] === newDId
+};
+
+
 export const isValid = (dbMM: DBMigrationsManager, isFirstRun) => {
   if (checkExecutedCount(isFirstRun ? 2 : 1)(dbMM)) {
     if (isFirstRun) {
@@ -28,10 +42,9 @@ export const isValid = (dbMM: DBMigrationsManager, isFirstRun) => {
     }
     return Promise.all(
       [
-        [getChangesetId(c1, fileName), MC.EXECTYPE_RERUN],
-        [getChangesetId(c2, fileName), MC.EXECTYPE_EXECUTED]
-      ].map(([id, execType]) => execTypeMatch(execType, [id])))
-      .then(results => results.every(r => r === true));
+        matchByTemplate([getChangesetId(c1, fileName)], t1),
+        matchByTemplate([getChangesetId(c2, fileName)], t2),
+      ]).then(results => results.every(r => r === true));
   }
   return false;
 };
