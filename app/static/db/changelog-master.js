7431939ff4199e3b785e93a650578be9319a00aa
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-11-22 18:43:09 +0200

    AMPOFFLINE-1312 configure default values for the new resource_type field

--- app/static/db/changelog-master.js
+++ app/static/db/changelog-master.js
@@ -1,6 +1,8 @@
 /**
  * This is a module to define all changelog files. Update 'changelogs' list in the right order when adding a new file.
  * So far we assume the files are defined under ./changelog folder.
+ *
+ * @see https://docs.google.com/document/d/1pVqC0a0PmvsjwBt6tZtF4uusQS7zTWbA-g1abomGfxg/edit?usp=sharing
  */
 
 const changelogs = [
