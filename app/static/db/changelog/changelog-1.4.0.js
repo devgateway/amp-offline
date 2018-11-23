ce47b2add609335a14b20ce3006f2347a6624eb0
Nadejda Mandrescu <17854915+nmandrescu@users.noreply.github.com>
2018-11-23 21:32:25 +0200

    AMPOFFLINE-1281 patch to force full currency rates resync if ran < 1.3

--- app/static/db/changelog/changelog-1.4.0.js
+++ app/static/db/changelog/changelog-1.4.0.js
@@ -1,6 +1,10 @@
 import * as RC from '../../../utils/constants/ResourceConstants';
 import { COLLECTION_RESOURCES } from '../../../utils/Constants';
 import * as Utils from '../../../utils/Utils';
+import * as CurrencyRatesHelper from '../../../modules/helpers/CurrencyRatesHelper';
+import * as MC from '../../../utils/constants/MigrationsConstants';
+import * as GSC from '../../../utils/constants/GlobalSettingsConstants';
+import * as GlobalSettingsHelper from '../../../modules/helpers/GlobalSettingsHelper';
 
 // AMPOFFLINE-1312-configure-web-link-resource_type
 const noResType = Utils.toMap(RC.RESOURCE_TYPE, { $exists: false });
@@ -11,6 +15,9 @@ linkFilter.$and.push(noResType);
 let docFilter = Utils.toUndefinedOrNullRule(RC.WEB_LINK);
 docFilter.$or.push(Utils.toDefinedNotNullRule(RC.CONTENT_ID));
 docFilter = { $and: [noResType, docFilter] };
+
+// AMPOFFLINE-1281
+let currentExchangeRates = [];
 
 export default({
   changelog: {
@@ -42,7 +49,31 @@ export default({
             filter: docFilter
           }
         }
-      }
+      },
+      {
+        changeid: 'AMPOFFLINE-1281',
+        author: 'nmandrescu',
+        comment: 'Force full resync for exchange rates app versions smaller than 1.3.0 was used',
+        preConditions: [{
+          func: () => CurrencyRatesHelper.findAll().then(dbCurrencyRates => {
+            currentExchangeRates = dbCurrencyRates;
+            return dbCurrencyRates.length > 0;
+          }),
+          onFail: MC.ON_FAIL_ERROR_MARK_RAN,
+          onError: MC.ON_FAIL_ERROR_CONTINUE
+        }, {
+          func: () => GlobalSettingsHelper.findByKey(GSC.DEFAULT_COUNTRY).then(dc =>
+            !!(dc && dc.value && ['ht', 'td'].includes(dc.value.toLowerCase()))),
+          onFail: MC.ON_FAIL_ERROR_CONTINUE,
+          onError: MC.ON_FAIL_ERROR_CONTINUE
+        }],
+        changes: {
+          func: () => CurrencyRatesHelper.replaceAllCurrencyRates([])
+        },
+        rollback: {
+          func: () => CurrencyRatesHelper.replaceAllCurrencyRates(currentExchangeRates)
+        }
+      },
     ]
   },
 });
