21bcfba1156d3ec56e35efe096bef9dbe7978b74
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2019-07-24 17:45:44 +0300

    AMPOFFLINE-1448 AMPOFFLINE-1477 sanity check page, actions, reducer

---
+++ app/actions/SanityCheckAction.js
@@ -0,0 +1,79 @@
+import { ipcRenderer } from 'electron';
+import store from '../index';
+import DatabaseSanityManager from '../modules/database/sanity/DatabaseSanityManager';
+import DatabaseSanityStatus from '../modules/database/sanity/DatabaseSanityStatus';
+import TranslationManager from '../modules/util/TranslationManager';
+import { initLanguage } from './TranslationAction';
+import Logger from '../modules/util/LoggerManager';
+import {
+  CLOSE_SANITY_APP,
+  FORCE_CLOSE_APP,
+  SHOW_SANITY_APP,
+  START_MAIN_APP
+} from '../utils/constants/ElectronAppMessages';
+
+
+const STATE_SANITY_CHECK = 'STATE_SANITY_CHECK';
+export const STATE_SANITY_CHECK_FULFILLED = 'STATE_SANITY_CHECK_FULFILLED';
+export const STATE_SANITY_CHECK_PENDING = 'STATE_SANITY_CHECK_PENDING';
+export const STATE_SANITY_CHECK_REJECTED = 'STATE_SANITY_CHECK_REJECTED';
+export const STATE_DB_HEAL_PROCEED = 'STATE_DB_HEAL_PROCEED';
+export const STATE_DB_HEAL_IN_PROGRESS = 'STATE_DB_HEAL_IN_PROGRESS';
+export const STATE_DB_HEAL_CANCEL = 'STATE_DB_HEAL_CANCEL';
+export const STATE_DB_HEAL_COMPLETE = 'STATE_DB_HEAL_COMPLETE';
+export const STATE_DB_HEAL_FAILURE_MSG_VIEWED = 'STATE_DB_HEAL_FAILURE_MSG_VIEWED';
+
+
+const logger = new Logger('SanityCheckAction');
+
+export const doSanityCheck = () => {
+  logger.log('doSanityCheck');
+  const sanityPromise = DatabaseSanityManager.sanityCheck();
+  store.dispatch({
+    type: STATE_SANITY_CHECK,
+    payload: sanityPromise
+  });
+  return sanityPromise.then((status: DatabaseSanityStatus) => {
+    if (status.isDBIncompatibilityDetected && !status.isHealedSuccessfully) {
+      logger.error('Database is corrupted');
+      ipcRenderer.send(SHOW_SANITY_APP);
+      return beforeSelfHealing().then(() => status);
+    } else {
+      logger.log('Cleanup not needed');
+      flagCleanupComplete(true);
+    }
+    return status;
+  });
+};
+
+const beforeSelfHealing = () => {
+  logger.log('beforeSelfHealing');
+  return TranslationManager.initializeTranslations(false).then(initLanguage);
+};
+
+export const doDBCleanup = (sanityStatus) => {
+  logger.log('doDBCleanup');
+  store.dispatch({ type: STATE_DB_HEAL_IN_PROGRESS });
+  return DatabaseSanityManager.cleanupDB(sanityStatus).then(result => {
+    store.dispatch({ type: STATE_DB_HEAL_COMPLETE });
+    return result;
+  });
+};
+
+export const cancelDBCleanup = (sanityStatus) => {
+  logger.log('cancelDBCleanup');
+  return DatabaseSanityManager.cancelDBCleanup(sanityStatus).then(result => {
+    flagCleanupComplete(false);
+    return result;
+  });
+};
+
+export const flagCleanupComplete = (isStartMainApp) => {
+  logger.log(`flagCleanupComplete: isStartMainApp = ${isStartMainApp}`);
+  ipcRenderer.send(CLOSE_SANITY_APP);
+  if (isStartMainApp) {
+    ipcRenderer.send(START_MAIN_APP);
+  } else {
+    ipcRenderer.send(FORCE_CLOSE_APP);
+  }
+};
---
+++ app/components/setup/Sanity.jsx
@@ -0,0 +1,131 @@
+/* eslint-disable react/no-unused-prop-types,class-methods-use-this */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as PropTypes from 'prop-types';
+import DatabaseSanityStatus from '../../modules/database/sanity/DatabaseSanityStatus';
+import { addConfirmationAlert } from '../../actions/NotificationAction';
+import NotificationsContainer from '../notifications';
+import Notification from '../../modules/helpers/NotificationHelper';
+import {
+  NOTIFICATION_ORIGIN_SANITY_CHECK,
+  NOTIFICATION_SEVERITY_ERROR
+} from '../../utils/constants/ErrorConstants';
+import FollowUp, { BUTTON_TYPE_CANCEL, BUTTON_TYPE_OK } from '../notifications/followup';
+import {
+  cancelDBCleanup,
+  doDBCleanup,
+  flagCleanupComplete,
+  STATE_DB_HEAL_CANCEL, STATE_DB_HEAL_FAILURE_MSG_VIEWED,
+  STATE_DB_HEAL_PROCEED
+} from '../../actions/SanityCheckAction';
+import translate from '../../utils/translate';
+import ConfirmationAlert from '../notifications/confirmationAlert';
+import Logger from '../../modules/util/LoggerManager';
+import InProgress from '../common/InProgress';
+
+const logger = new Logger('Sanity');
+
+/**
+ * Sanity window
+ * @author Nadejda Mandrescu
+ */
+class Sanity extends Component {
+  static propTypes = {
+    sanityStatus: PropTypes.instanceOf(DatabaseSanityStatus).isRequired,
+    onDBHealingConfirmationAlert: PropTypes.func.isRequired,
+    isPerformDBCleanup: PropTypes.bool,
+    isDBCleanupInProgress: PropTypes.bool,
+    isCancelDBCleanup: PropTypes.bool,
+    isDBCleanupCompleted: PropTypes.bool,
+    isDBFailureMsgViewed: PropTypes.bool,
+  };
+
+  componentWillMount() {
+    this.checkNewProps(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.checkNewProps(nextProps);
+  }
+
+  checkNewProps(props) {
+    const {
+      sanityStatus, isPerformDBCleanup, isDBCleanupInProgress, isCancelDBCleanup, isDBCleanupCompleted,
+      isDBFailureMsgViewed, onDBHealingConfirmationAlert
+    } = props;
+    if (isDBCleanupCompleted) {
+      if (!sanityStatus.isHealedSuccessfully && !isDBFailureMsgViewed) {
+        onDBHealingConfirmationAlert(sanityStatus, true);
+      } else {
+        flagCleanupComplete(sanityStatus.isHealedSuccessfully);
+      }
+    } else if (isCancelDBCleanup) {
+      logger.warn('DB cleanup was canceled by the user. Closing the app.');
+      cancelDBCleanup(sanityStatus);
+    } else if (isPerformDBCleanup) {
+      if (!isDBCleanupInProgress) {
+        logger.warn('Starting DB cleanup');
+        doDBCleanup(sanityStatus);
+      }
+    } else {
+      logger.log('Notifying user about DB corruption');
+      onDBHealingConfirmationAlert(sanityStatus);
+    }
+  }
+
+  render() {
+    const { isDBCleanupInProgress } = this.props;
+    return (
+      <div>
+        <NotificationsContainer asModal={false} />
+        {isDBCleanupInProgress && <InProgress />}
+      </div>
+    );
+  }
+
+}
+
+const dbHealingConfirmationAlert = (databaseSanityStatus: DatabaseSanityStatus, isOnFailure = false) => {
+  let message;
+  let okMsg = 'ok';
+  if (isOnFailure) {
+    message = 'dbCleanupFailed';
+  } else if (databaseSanityStatus.isDBIncompatibilityExpected) {
+    message = 'dbCompatibilityError';
+  } else {
+    message = 'dbCorrupted';
+    okMsg = 'cleanup';
+  }
+  const dbHealNotification = new Notification({
+    message,
+    origin: NOTIFICATION_ORIGIN_SANITY_CHECK,
+    severity: NOTIFICATION_SEVERITY_ERROR
+  });
+
+  const proceed = new FollowUp({
+    type: isOnFailure ? STATE_DB_HEAL_FAILURE_MSG_VIEWED : STATE_DB_HEAL_PROCEED,
+  }, translate(okMsg), BUTTON_TYPE_OK);
+  const actions = [proceed];
+  if (!isOnFailure && !databaseSanityStatus.isDBIncompatibilityExpected) {
+    const cancel = new FollowUp({
+      type: STATE_DB_HEAL_CANCEL,
+    }, translate('cancel'), BUTTON_TYPE_CANCEL);
+    actions.push(cancel);
+  }
+  return new ConfirmationAlert(dbHealNotification, actions, false, translate('error'));
+};
+
+export default connect(
+  state => ({
+    sanityStatus: state.sanityCheckReducer.databaseSanityStatus,
+    isPerformDBCleanup: state.sanityCheckReducer.isPerformDBCleanup,
+    isDBCleanupInProgress: state.sanityCheckReducer.isDBCleanupInProgress,
+    isCancelDBCleanup: state.sanityCheckReducer.isCancelDBCleanup,
+    isDBCleanupCompleted: state.sanityCheckReducer.isDBCleanupCompleted,
+    isDBFailureMsgViewed: state.sanityCheckReducer.isDBFailureMsgViewed,
+  }),
+  dispatch => ({
+    onDBHealingConfirmationAlert: (databaseSanityStatus, isOnFailure) =>
+      dispatch(addConfirmationAlert(dbHealingConfirmationAlert(databaseSanityStatus, isOnFailure)))
+  })
+)(Sanity);
