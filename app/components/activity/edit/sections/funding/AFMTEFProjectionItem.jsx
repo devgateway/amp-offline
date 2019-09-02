772cc2fa0b34cd24e7efb58d644d17badb992eb3
Gabriel Inchauspe <ginchauspe@gmail.com>
2019-09-02 11:58:53 -0300

    AMPUI-69: Change constant.

--- app/components/activity/edit/sections/funding/AFMTEFProjectionItem.jsx
+++ app/components/activity/edit/sections/funding/AFMTEFProjectionItem.jsx
@@ -37,7 +37,7 @@ class AFMTEFProjectionItem extends Component {
     const { activityFieldsManager, currentWorkspaceSettings, currencyRatesManager } = this.context;
     // When adding a new item we select the default currency like in AMP.
     if (!mtefItem[ActivityConstants.CURRENCY].id) {
-      const currencies = activityFieldsManager.getPossibleValuesOptions(FieldPathConstants.FUNDING_CURRENCY_PATH);
+      const currencies = activityFieldsManager.getPossibleValuesOptions(FieldPathConstants.MTEF_CURRENCY_PATH);
       const wsCurrencyCode = currentWorkspaceSettings.currency.code;
       const currency = AFUtils.getDefaultOrFirstUsableCurrency(currencies, wsCurrencyCode, currencyRatesManager);
       mtefItem[ActivityConstants.CURRENCY] = currency;
