05cf8dd9995abe5a58e9ce41236e769068649d25
Gabriel Inchauspe <ginchauspe@gmail.com>
2020-04-08 15:43:04 -0300

    AMPOFFLINE-1515: Move some logic to preconditions.

--- app/static/db/changelog/changelog-1.4.1.js
+++ app/static/db/changelog/changelog-1.4.1.js
@@ -14,6 +14,10 @@ import { ACTIVITY_LOCATION_FIX_OLD_IDS, AMP_COUNTRY_FLAG } from '../../../module
 import * as ConnectionHelper from '../../../modules/connectivity/ConnectionHelper';
 import * as DatabaseManager from '../../../modules/database/DatabaseManager';
 
+// AMPOFFLINE-1515.
+let newAndOldLocationIds = [];
+let activitiesTable;
+
 export default ({
   changelog: {
     preConditions: [],
@@ -22,35 +26,48 @@ export default ({
         changeid: 'AMPOFFLINE-1515-update-location-ids',
         author: 'ginchauspe',
         comment: 'Go online to retrieve new ids for amp locations.',
-        preConditions: [],
+        preConditions: [
+          {
+            // Get new and old location ids from the EP.
+            func: () => ConnectionHelper.doGet({ url: ACTIVITY_LOCATION_FIX_OLD_IDS, shouldRetry: true })
+              .then(data => {
+                newAndOldLocationIds = data;
+                return newAndOldLocationIds.length > 0;
+              }),
+            onFail: MC.ON_FAIL_ERROR_MARK_RAN,
+            onError: MC.ON_FAIL_ERROR_CONTINUE
+          },
+          {
+            // Open "table" activities.
+            func: () => DatabaseManager._getCollection(Constants.COLLECTION_ACTIVITIES)
+              .then(collection => {
+                activitiesTable = collection;
+                return activitiesTable !== undefined;
+              }),
+            onFail: MC.ON_FAIL_ERROR_MARK_RAN,
+            onError: MC.ON_FAIL_ERROR_CONTINUE
+          }
+        ],
         context: MC.CONTEXT_AFTER_LOGIN,
         changes: [{
           func: () => {
-            return ConnectionHelper.doGet({ url: ACTIVITY_LOCATION_FIX_OLD_IDS, shouldRetry: true }).then((data) => {
-              logger.error(data);
-              const errors = false;
-              if (data) {
-                DatabaseManager._getCollection(Constants.COLLECTION_ACTIVITIES)
-                  .then(collection => {
-                    DatabaseManager.createIndex(collection, { fieldName: 'locations.location' });
-                    let i = -1;
-                      // TODO: Create a promise for all subqueries.
-                    data.forEach(d => {
-                      i++;
-                      console.error(d);
-                      console.info(i);
-                        // NOTE: locations: { $exists: true } makes the query slower.
-                      return ActivityHelper.findAll({ 'locations.location': d.extra_info.old_location_id }).then(activities => {
-                        if (activities.length > 0) {
-                          console.info(activities);
-                        }
-                        return activities;
-                      });
-                    });
-                  });
-              }
-              throw new Error();
+            const errors = false;
+            DatabaseManager.createIndex(activitiesTable, { fieldName: 'locations.location' });
+            let i = -1;
+            newAndOldLocationIds.forEach(d => {
+              i++;
+              console.error(d);
+              console.info(i);
+              // NOTE: locations: { $exists: true } makes the query slower.
+              return ActivityHelper.findAll({ 'locations.location': d.extra_info.old_location_id })
+                          .then(activities => {
+                            if (activities.length > 0) {
+                              console.info(activities);
+                            }
+                            return activities;
+                          });
             });
+            throw new Error();
           }
         }],
         rollback: {
