d5fcdbe0f2fdba3693062e2245dd7e165d64dd2c
Gabriel Inchauspe <ginchauspe@gmail.com>
2020-04-10 16:43:09 -0300

    AMPOFFLINE-1515: Added 2nd patch to cleanup old possible-values.

--- app/static/db/changelog/changelog-1.4.1.js
+++ app/static/db/changelog/changelog-1.4.1.js
@@ -1,42 +1,41 @@
 /* eslint-disable max-len */
-import { ActivityConstants, Constants, FieldPathConstants, AllApprovalStatuses, GlobalSettingsConstants } from 'amp-ui';
-import * as RC from '../../../utils/constants/ResourceConstants';
-import * as Utils from '../../../utils/Utils';
-import * as CurrencyRatesHelper from '../../../modules/helpers/CurrencyRatesHelper';
+import { Constants, FieldPathConstants } from 'amp-ui';
 import * as MC from '../../../utils/constants/MigrationsConstants';
-import * as GlobalSettingsHelper from '../../../modules/helpers/GlobalSettingsHelper';
 import * as CSC from '../../../utils/constants/ClientSettingsConstants';
-import PossibleValuesHelper from '../../../modules/helpers/PossibleValuesHelper';
 import * as ActivityHelper from '../../../modules/helpers/ActivityHelper';
 import logger from '../ChangelogLogger';
-import DateUtils from '../../../utils/DateUtils';
-import { ACTIVITY_LOCATION_FIX_OLD_IDS, AMP_COUNTRY_FLAG } from '../../../modules/connectivity/AmpApiConstants';
+import { ACTIVITY_LOCATION_FIX_OLD_IDS } from '../../../modules/connectivity/AmpApiConstants';
 import * as ConnectionHelper from '../../../modules/connectivity/ConnectionHelper';
 import * as DatabaseManager from '../../../modules/database/DatabaseManager';
+import * as ClientSettingsHelper from '../../../modules/helpers/ClientSettingsHelper';
+import PossibleValuesHelper from '../../../modules/helpers/PossibleValuesHelper';
 
 // AMPOFFLINE-1515.
 let newAndOldLocationIds = [];
 let activitiesTable;
+const getNewLocationIds = () => (
+  ConnectionHelper.doGet({ url: ACTIVITY_LOCATION_FIX_OLD_IDS, shouldRetry: true })
+    .then(data => {
+      logger.info(`Got data from ${ACTIVITY_LOCATION_FIX_OLD_IDS}`);
+      logger.info(data); // TODO: remove this line.
+      newAndOldLocationIds = data;
+      return newAndOldLocationIds.length > 0;
+    })
+);
 
 export default ({
   changelog: {
     preConditions: [],
     changesets: [
       {
-        changeid: 'AMPOFFLINE-1515-update-location-ids',
+        changeid: 'AMPOFFLINE-1515-update-location-ids-activities',
         author: 'ginchauspe',
-        comment: 'Go online to retrieve new ids for amp locations.',
+        comment: 'Go online to retrieve new ids for amp locations (Part 1 of 2)',
         preConditions: [
           {
             // Get new and old location ids from the EP.
             // TODO: First check if we have any data in activities to save time.
-            func: () => ConnectionHelper.doGet({ url: ACTIVITY_LOCATION_FIX_OLD_IDS, shouldRetry: true })
-              .then(data => {
-                logger.info(`Got data from ${ACTIVITY_LOCATION_FIX_OLD_IDS}`);
-                logger.info(data);
-                newAndOldLocationIds = data;
-                return newAndOldLocationIds.length > 0;
-              }),
+            func: () => getNewLocationIds(),
             onFail: MC.ON_FAIL_ERROR_MARK_RAN,
             onError: MC.ON_FAIL_ERROR_CONTINUE
           },
@@ -56,23 +55,46 @@ export default ({
         context: MC.CONTEXT_AFTER_LOGIN,
         changes: [{
           func: () => {
-            // TODO: Update locations in possible-values.
             const activitiesToUpdate = [];
             return Promise.all(newAndOldLocationIds.map(d =>
-              ActivityHelper.findAll({ 'locations.location': d.extra_info.old_location_id })
+              // NOTE: Filtering first by 'locations: $exists' made it slower.
+              ActivityHelper.findAll({ 'locations.location': d['extra_info'/* FieldPathConstants.EXTRA_INFO*/].old_location_id })
                 .then(activities => {
                   if (activities.length > 0) {
                     activities.forEach(a => {
-                      a.locations.filter(l => l.location === d.extra_info.old_location_id).forEach(l => {
-                        console.info(a);
-                        l.location = d.id;
-                      });
+                      a.locations.filter(l => l.location === d['extra_info'/* FieldPathConstants.EXTRA_INFO*/].old_location_id)
+                        .forEach(l => {
+                          console.info(a); // TODO: remove this line.
+                          l.location = d.id;
+                        });
                       activitiesToUpdate.push(a);
                     });
                   }
                   return Promise.resolve();
                 }))).then(() => ActivityHelper.saveOrUpdateCollection(activitiesToUpdate));
           }
+        }],
+        rollback: {
+          func: () => logger.error('rollback')
+        }
+      },
+      {
+        changeid: 'AMPOFFLINE-1515-update-location-ids-locations',
+        author: 'ginchauspe',
+        comment: 'Go online to retrieve new ids for amp locations (Part 2 of 2)',
+        preConditions: [
+          {
+            changeid: 'AMPOFFLINE-1515-update-location-ids-activities',
+            author: 'ginchauspe',
+            file: 'changelog-1.4.1.js',
+            onFail: MC.ON_FAIL_ERROR_CONTINUE,
+            onError: MC.ON_FAIL_ERROR_CONTINUE
+          }
+        ],
+        context: MC.CONTEXT_AFTER_LOGIN,
+        changes: [{
+          func: () => Promise.all([PossibleValuesHelper.deleteById('locations~location'),
+            ClientSettingsHelper.updateSettingValue(CSC.FORCE_SYNC_UP, true)])
         }],
         rollback: {
           func: () => logger.error('rollback')
