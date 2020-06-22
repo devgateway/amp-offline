7445092304d16dc41dfc1beed46519da98c07451
Gabriel Inchauspe <ginchauspe@gmail.com>
2020-06-22 16:24:30 -0300

    AMPOFFLINE-1519: Add search.

--- app/components/activity/edit/sections/AFM_E.jsx
+++ app/components/activity/edit/sections/AFM_E.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import * as PropTypes from 'prop-types';
-import { Col, Grid, Row, Panel } from 'react-bootstrap';
+import { Col, Grid, Panel, Row } from 'react-bootstrap';
 import { ActivityConstants, FeatureManagerConstants } from 'amp-ui';
 import afStyles from '../ActivityForm.css';
 import styles from './funding/AFFundingDetailItem.css';
@@ -9,6 +9,7 @@ import AFField from '../components/AFField';
 import { M_E } from './AFSectionConstants';
 import Logger from '../../../../modules/util/LoggerManager';
 import fundingStyles from './funding/AFFundingContainer.css';
+import * as AFComponentTypes from '../components/AFComponentTypes';
 
 const logger = new Logger('AF M&E');
 
@@ -27,6 +28,7 @@ class AFM_E extends Component {
     }
     this._handlePanelOpenClose = this._handlePanelOpenClose.bind(this);
     this.findPanel = this.findPanel.bind(this);
+    this.handleAddM_E = this.handleAddM_E.bind(this);
   }
 
   _handlePanelOpenClose(id) {
@@ -40,13 +42,25 @@ class AFM_E extends Component {
     return activity[ActivityConstants.INDICATORS].find(p => p.id === id);
   }
 
-  render() {
+  // eslint-disable-next-line camelcase
+  handleAddM_E(id) {
+    debugger;
     const { activity } = this.props;
     const indicators = activity[ActivityConstants.INDICATORS] || [];
-    return (<div className={afStyles.full_width} >
-      <Grid className={afStyles.full_width} >
+    if (!indicators.find(i => i.indicator.id === id)) {
+
+    }
+    activity[ActivityConstants.INDICATORS] = indicators;
+  }
+
+  render() {
+    const { activity } = this.props;
+    console.error(activity);
+    const indicators = activity[ActivityConstants.INDICATORS] || [];
+    return (<div className={afStyles.full_width}>
+      <Grid className={afStyles.full_width}>
         <Row>
-          <Col md={12} lg={12} >
+          <Col md={12} lg={12}>
             {indicators.map(i => {
               const errors = ActivityConstants.ME_SECTIONS.some(j => i[j] && i[j].errors && i[j].errors.length > 0);
               return (<Panel
@@ -60,40 +74,51 @@ class AFM_E extends Component {
                     fmPath={FeatureManagerConstants.ME_ITEM_LOGFRAME_CATEGORY} />
                   <hr />
                   {ActivityConstants.ME_SECTIONS
-                    .map(s => (i[s] ? <table className={styles.full_width} key={Math.random()}><tbody>
-                      <tr>
-                        <td className={styles.row}>
-                          <div className={styles.row}>
-                            <AFField
-                              parent={i[s]}
-                              customLabel={`${s} value`}
-                              className={styles.cell_2}
-                              fieldPath={`${ActivityConstants.INDICATORS}~${s}~${ActivityConstants.INDICATOR_VALUE}`} />
-                            <AFField
-                              parent={i[s]}
-                              customLabel={`${s} date`}
-                              className={styles.cell_2}
-                              fieldPath={`${ActivityConstants.INDICATORS}~${s}~${ActivityConstants.INDICATOR_DATE}`} />
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <div className={styles.row}>
-                            <AFField
-                              parent={i[s]}
-                              fieldPath={`${ActivityConstants.INDICATORS}~${s}~${ActivityConstants.INDICATOR_COMMENT}`}
-                              customLabel={`${s} comment`} />
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody></table> : null))}
+                    .map(s => (i[s] ? <table className={styles.full_width} key={Math.random()}>
+                      <tbody>
+                        <tr>
+                          <td className={styles.row}>
+                            <div className={styles.row}>
+                              <AFField
+                                parent={i[s]}
+                                customLabel={`${s} value`}
+                                className={styles.cell_2}
+                                // eslint-disable-next-line max-len
+                                fieldPath={`${ActivityConstants.INDICATORS}~${s}~${ActivityConstants.INDICATOR_VALUE}`} />
+                              <AFField
+                                parent={i[s]}
+                                customLabel={`${s} date`}
+                                className={styles.cell_2}
+                                // eslint-disable-next-line max-len
+                                fieldPath={`${ActivityConstants.INDICATORS}~${s}~${ActivityConstants.INDICATOR_DATE}`} />
+                            </div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <div className={styles.row}>
+                              <AFField
+                                parent={i[s]}
+                                // eslint-disable-next-line max-len
+                                fieldPath={`${ActivityConstants.INDICATORS}~${s}~${ActivityConstants.INDICATOR_COMMENT}`}
+                                customLabel={`${s} comment`} />
+                            </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table> : null))}
                 </div>
                 <AFField parent={i} fieldPath={`${ActivityConstants.INDICATORS}~${ActivityConstants.RISK}`} />
               </Panel>);
             })}
           </Col>
         </Row>
+        <Col md={12} lg={12}>
+          <AFField
+            parent={this.props.activity} fieldPath={`${ActivityConstants.INDICATORS}~${ActivityConstants.INDICATOR}`}
+            type={AFComponentTypes.SEARCH} onAfterUpdate={this.handleAddM_E} />
+        </Col>
+        <Row />
       </Grid>
     </div>);
   }
