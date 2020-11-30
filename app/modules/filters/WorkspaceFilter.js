d4b46ff5172f6180beb72eed0b88c9cc3c24103e
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2020-11-30 10:14:14 -0300

    AMPOFFLINE-1548 fix ws filters

--- app/modules/filters/WorkspaceFilter.js
+++ app/modules/filters/WorkspaceFilter.js
@@ -143,24 +143,24 @@ export default class WorkspaceFilterBuilder {
     return new Promise((resolve, reject) =>
       wsIds.reduce((promise, wsId) =>
         promise.then(() => new Promise(() => {
-            if (!processedWsIds.has(wsId)) {
-              logger.log(wsId);
-              processedWsIds.add(wsId);
+          if (!processedWsIds.has(wsId)) {
+            logger.log(wsId);
+            processedWsIds.add(wsId);
 
-              WorkspaceHelper.findAll({ 'parent-workspace-id': wsId }, { id: 1 }).then(childWsIds => {
-                const idsList = childWsIds.map(ws => ws.id);
-                return self._getChildrenWorkspaces(idsList, processedWsIds).then(result => resolve(result));
-              }).catch(reject);
-            }
-            return processedWsIds;
-          }),
-          Promise.resolve(processedWsIds)).then(() => {
-          logger.log('done');
+            WorkspaceHelper.findAll({ 'parent-workspace-id': wsId }, { id: 1 }).then(childWsIds => {
+              const idsList = childWsIds.map(ws => ws.id);
+              return self._getChildrenWorkspaces(idsList, processedWsIds).then(result => resolve(result));
+            }).catch(reject);
+          }
           return processedWsIds;
-        }, (e) => {
-          logger.error(e);
-          reject(e);
-        })
+        }),
+          Promise.resolve(processedWsIds)).then(() => {
+            logger.log('done');
+            return processedWsIds;
+          }, (e) => {
+            logger.error(e);
+            reject(e);
+          })
       ));
   }
 }
