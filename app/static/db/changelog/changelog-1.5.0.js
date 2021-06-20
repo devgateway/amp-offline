546a48ec719b5af7cdf435ce35e72ed3fa1cf518
ginchauspe <ginchauspe@gmail.com>
2021-06-20 19:39:44 -0300

    AMPOFFLINE-1531: Simplify the upgrade process and rely on FMSyncUpManager.

--- app/static/db/changelog/changelog-1.5.0.js
+++ app/static/db/changelog/changelog-1.5.0.js
@@ -1,20 +1,9 @@
 /* eslint-disable max-len */
-import { FeatureManagerConstants, FmManagerHelper } from 'amp-ui';
 import PossibleValuesHelper from '../../../modules/helpers/PossibleValuesHelper';
 import * as MC from '../../../utils/constants/MigrationsConstants';
 import * as CSC from '../../../utils/constants/ClientSettingsConstants';
 import * as ClientSettingsHelper from '../../../modules/helpers/ClientSettingsHelper';
 import FMHelper from '../../../modules/helpers/FMHelper';
-import { FEATURE_MANAGER_BY_WS_URL } from '../../../modules/connectivity/AmpApiConstants';
-
-const body = FmManagerHelper.getRequestFmSyncUpBody(Object.values(FeatureManagerConstants));
-
-// AMPOFFLINE-1515: DO NOT LOAD ConnectionHelper WITH IMPORT BECAUSE IT WILL BREAK MOCHA-TESTS.
-let ConnectionHelper = {};
-if (process.env.NODE_ENV !== 'test') {
-// eslint-disable-next-line global-require
-  ConnectionHelper = require('../../../modules/connectivity/ConnectionHelper');
-}
 
 export default ({
   changelog: {
@@ -39,8 +28,7 @@ export default ({
         context: MC.CONTEXT_AFTER_LOGIN,
         changes: [{
           func: () => Promise.all([ClientSettingsHelper.updateSettingValue(CSC.FORCE_SYNC_UP, true),
-            FMHelper.removeAll({}).then(() => ConnectionHelper.doPost({ url: FEATURE_MANAGER_BY_WS_URL, body, shouldRetry: true })
-              .then((fmTree) => FMHelper.replaceAll([{ fmTree }])))])
+            FMHelper.removeAll({})])
         }]
       }
     ]
